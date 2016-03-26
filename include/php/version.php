9e62396e41d9a98f1143f02848c6974e373f0307
Joshua Torrance <torrancj@gmail.com>
2016-03-26 08:04:48 -0600

    Testing out version control system to update cache

---
+++ include/php/version.php
@@ -0,0 +1,2 @@
+<?php
+echo "?version_1.0.0";
---
+++ practicetools/albertadrugprice_test/details.php
@@ -0,0 +1,4 @@
+<?php
+	$title = 'Alberta Drug Price Calculator';
+	$description = 'Calculates the cost of a list of medications for your patient. Also identifies any requirements for drug coverage under Alberta Blue Cross.';
+	$update = '2015-12-12';
