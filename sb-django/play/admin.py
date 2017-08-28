4db504f614fe30b2ef84a111d3cef1815cd30d1b
studybuffalo <studybuffalo@gmail.com>
2017-08-28 15:07:34 -0600

    Removing old code from admin file

--- sb-django/play/admin.py
+++ sb-django/play/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import PlayPage, PlayImage, PlayAudio, Category
 
-#admin.site.register(PlayPage)
 admin.site.register(PlayImage)
 admin.site.register(PlayAudio)
 admin.site.register(Category)
