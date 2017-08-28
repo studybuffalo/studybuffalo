1b5d0e65032d5c7ccc55feaf31dc0eb086b8977d
studybuffalo <studybuffalo@gmail.com>
2017-08-28 12:40:43 -0600

    Updating admin site to allow inline entry of PlayPage items

--- sb-django/play/admin.py
+++ sb-django/play/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import PlayPage, PlayImage, PlayAudio, Category
 
-admin.site.register(PlayPage)
+#admin.site.register(PlayPage)
 admin.site.register(PlayImage)
 admin.site.register(PlayAudio)
 admin.site.register(Category)
+
+class PlayImageInline(admin.StackedInline):
+    model = PlayImage
+    extra = 0
+    fields = ["original_image", ("title", "alt_text", "ordering"), "description", ]
+
+class PlayAudioInline(admin.StackedInline):
+    model = PlayAudio
+    extra = 0
+
+@admin.register(PlayPage)
+class PlayPageAdmin(admin.ModelAdmin):
+    list_display = ("date", "title", "release_date", "category")
+    list_filter = ("date", "release_date", "category")
+    fields = ["title", ("date", "release_date"), "category"]
+    inlines = [PlayImageInline, PlayAudioInline]
