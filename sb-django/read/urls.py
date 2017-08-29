f34e089274f751d48c8582bb8f6251b996ef726c
studybuffalo <studybuffalo@gmail.com>
2017-08-28 21:04:35 -0600

    Adding URL, Views, and templates for the read section

--- sb-django/read/urls.py
+++ sb-django/read/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.Index.as_view(), name="study_index"),
-    url(r"^(?P<pk>\d+)/$", views.GuideDetail.as_view(), name="study_guide_page"),
+    url(r"^$", views.Index.as_view(), name="pub_index"),
+    url(r"^(?P<pk>\d+)/$", views.PublicationDetail.as_view(), name="pub_page"),
 ]
--- sb-django/read/views.py
+++ sb-django/read/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Publication
 
-# Create your views here.
+class Index(generic.ListView):
+    model = Publication
+    context_object_name = "pub_list"
+    template_name = "read/index.html"
+
+class PublicationDetail(generic.DetailView):
+    model = Publication
+
+    context_object_name = "publication_page"
