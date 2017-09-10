daf3c09745a902b859d33a84bb93869f2180d1ad
studybuffalo <studybuffalo@gmail.com>
2017-09-09 23:19:09 -0600

    Adding twitter support for authentication

--- sb-django/sb_django/settings.py
+++ sb-django/sb_django/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.instagram',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.twitter',
