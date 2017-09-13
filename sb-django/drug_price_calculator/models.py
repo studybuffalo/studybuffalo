949a8fbaf8111598f1d5f1d95f2811b1fbc236da
studybuffalo <studybuffalo@gmail.com>
2017-09-13 14:33:12 -0600

    Adding planning details to the drug price calculator

--- sb-django/drug_price_calculator/models.py
+++ sb-django/drug_price_calculator/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+"""
+    Planning for migration to Django
+    
+    List of medications
+    Trade name
+    Brand Name
+    Ingredients +/- strengths +/- dosage form (EPID?)
+    ptcFK
+    atcFK
+
+    Price info
+        medicationFK
+        unit price
+        unit of issue
+        LCA
+        MAC
+        source
+            ABC iDBL
+            ABC DPL
+            NIHB
+
+    PTC List
+        
+    ATC List
+
+    Coverage
+        ABC coverage
+            Plan coverage amounts
+            ABC special auth
+        NIHB coverage
+            Prior approval
+"""
 # Create your models here.
