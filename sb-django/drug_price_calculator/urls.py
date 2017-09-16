a6b546e8e5e4b825dfa9daf3622968e7093dbb6e
Joshua Torrance <torrancj@gmail.com>
2017-09-16 16:03:05 -0600

    Adding in list views to review database contents

--- sb-django/drug_price_calculator/urls.py
+++ sb-django/drug_price_calculator/urls.py
@@ -2,5 +2,17 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r"^atc/$", views.ATCList.as_view(), name="atc_list"),
+    url(r"^coverage/$", views.CoverageList.as_view(), name="coverage_list"),
+    url(r"^extra-information/$", views.ExtraInformationList.as_view(), name="extra_information_list"),
+    url(r"^price/$", views.PriceList.as_view(), name="price_list"),
+    url(r"^ptc/$", views.PTCList.as_view(), name="ptc_list"),
+    url(r"^subs/atc/$", views.SubsATCList.as_view(), name="subs_atc_list"),
+    url(r"^subs/bsrf/$", views.SubsBSRFList.as_view(), name="subs_bsrf_list"),
+    url(r"^subs/generic/$", views.SubsGenericList.as_view(), name="subs_generic_list"),
+    url(r"^subs/manufacturer/$", views.SubsManufacturerList.as_view(), name="subs_manufacturer_list"),
+    url(r"^subs/ptc/$", views.SubsPTCList.as_view(), name="subs_ptc_list"),
+    url(r"^subs/unit/$", views.SubsUnitList.as_view(), name="subs_unit_list"),
+    url(r"^special-authorization/$", views.SpecialAuthorizationList.as_view(), name="special_authorization_list"),
     url(r"^$", views.index, name="drug_price_calculator_index"),
 ]
--- sb-django/drug_price_calculator/views.py
+++ sb-django/drug_price_calculator/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import (ATC, Coverage, ExtraInformation, Price, PTC, 
+                     SpecialAuthorization, ATCDescriptions, SubsBSRF, 
+                     SubsGeneric, SubsManufacturer, SubsPTC, SubsUnit)
 
 def index(request):
     """View for the main drug price calculator page"""
@@ -7,3 +11,64 @@ def index(request):
         "drug_price_calculator/index.html",
         context={},
     )
+
+
+class ATCList(generic.ListView):
+    model = ATC
+
+    context_object_name = "atc_list"
+
+class CoverageList(generic.ListView):
+    model = Coverage
+
+    context_object_name = "coverage_list"
+
+class ExtraInformationList(generic.ListView):
+    model = ExtraInformation
+
+    context_object_name = "extra_information_list"
+
+class PriceList(generic.ListView):
+    model = Price
+
+    context_object_name = "price_list"
+
+class PTCList(generic.ListView):
+    model = PTC
+
+    context_object_name = "ptc_list"
+
+class SpecialAuthorizationList(generic.ListView):
+    model = SpecialAuthorization
+
+    context_object_name = "special_authorization_list"
+
+class SubsATCList(generic.ListView):
+    model = ATCDescriptions
+
+    context_object_name = "subs_atc_list"
+
+class SubsBSRFList(generic.ListView):
+    model = SubsBSRF
+
+    context_object_name = "subs_bsrf_list"
+
+class SubsGenericList(generic.ListView):
+    model = SubsGeneric
+
+    context_object_name = "subs_generic_list"
+
+class SubsManufacturerList(generic.ListView):
+    model = SubsManufacturer
+
+    context_object_name = "subs_manufacturer_list"
+
+class SubsPTCList(generic.ListView):
+    model = SubsPTC
+    
+    context_object_name = "subs_ptc_list"
+
+class SubsUnitList(generic.ListView):
+    model = SubsUnit
+
+    context_object_name = "subs_unit_list"
