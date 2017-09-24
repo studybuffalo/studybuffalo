f26dc10d45cf42109331b771300c5a8240cf536d
studybuffalo <studybuffalo@gmail.com>
2017-09-24 17:55:35 -0600

    Updating updates admin list display

--- sb-django/updates/admin.py
+++ sb-django/updates/admin.py
@@ -4,5 +4,6 @@ from .models import Update
 
 @admin.register(Update)
 class UpdateSlideAdmin(admin.ModelAdmin):
-    list_display = ("start_date", "end_date", "priority", "background")
-    list_filter = ("start_date", "end_date")
+    list_display = ("priority", "title", "start_date", "end_date",)
+    list_filter = ("start_date", "end_date",)
+    ordering = ("priority",)
