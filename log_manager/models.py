dd3e0d16d93e209329ae2b60f95ec68cfe7107f5
Joshua Torrance <torrancj@gmail.com>
2017-10-08 09:40:53 -0600

    Creating the models for the app

--- log_manager/models.py
+++ log_manager/models.py
@@ -1,3 +1,195 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+
+from datetime import datetime
 
 # Create your models here.
+
+class AppData(models.Model):
+    """Records information on apps to collect log data on"""
+    name = models.CharField(
+    )
+
+    log_location = models.CharField(
+    )
+
+    file_name = models.CharField(
+    )
+
+    update_minutes = models.PositiveSmallIntegerField(
+        blank=True,
+        help_text="The minutes of an hour to update on",
+        null=True,
+        validators=[
+            MaxValueValidator(59),
+        ]
+    )
+
+    update_hours = models.PositiveSmallIntegerField(
+        blank=True,
+        help_text="The hour of the day to update on (0 = midnight)",
+        null=True,
+        validators=[
+            MaxValueValidator(59),
+        ]
+    )
+
+    update_days = models.PositiveSmallIntegerField(
+        blank=True,
+        help_text="The day of the month to update on (1-31)",
+        null=True,
+        validators=[
+            MaxValueValidator(59),
+        ]
+    )
+
+    update_month = models.PositiveSmallIntegerField(
+        blank=True,
+        help_text="The month to update on (1 = January, 12 = December)",
+        null=True,
+        validators=[
+            MaxValueValidator(59),
+        ]
+    )
+
+    update_weekday = models.PositiveSmallIntegerField(
+        blank=True,
+        help_text="The weekday to update on (0 = Sunday, 6 = Saturday)",
+        null=True,
+        validators=[
+            MaxValueValidator(6),
+        ]
+    )
+
+    last_update = models.DateTimeField(
+        auto_now=True,
+    )
+
+    next_update = models.DateTimeField(
+        default=datetime.now(),
+    )
+
+class LogEntry(models.Model):
+    """Records a single log entry on an app"""
+    
+    app_name = models.ForeignKey(
+        AppData,
+        help_text="The app this entry is for",
+    )
+
+    asc_time = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+
+    created = models.TimeField(
+        blank=True,
+        null=True,
+    )
+
+    exc_info = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    file_name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    func_name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    level_name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    level_no = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    line_no = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+    )
+
+    module = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    msecs = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+    )
+
+    message = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    path_name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    process = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    process_name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    relative_created = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+    )
+
+    stack_info = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    thread = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+    thread_name = models.CharField(
+        blank=True,
+        null=True,
+    )
+
+"""Matching Django DB names against logging formatter names
+        asc_time            asctime         %(asctime)s
+        created             created	        %(created)f
+        exc_info            exc_info
+        file_name           filename	    %(filename)s
+        func_name           funcName	    %(funcName)s
+        level_name          levelname	    %(levelname)s
+        level_no            levelno	        %(levelno)s
+        line_no             lineno	        %(lineno)d
+        message             message	        %(message)s
+        module              module	        %(module)s
+        msecs               msecs	        %(msecs)d
+        name                name	        %(name)s
+        path_name           pathname        %(pathname)s
+        process             process	        %(process)d
+        process_name        processName     %(processName)s
+        relative_created    relativeCreated %(relativeCreated)d
+        stack_info          stack_info
+        thread              thread	        %(thread)d
+        thread_name         threadName	    %(threadName)s
+    """
