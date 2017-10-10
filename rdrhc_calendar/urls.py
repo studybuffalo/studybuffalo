a43fd54b6d9d651da45bf28edeec40f6e7774333
studybuffalo <studybuffalo@gmail.com>
2017-10-09 20:23:17 -0600

    Creating a separate page for the "how-to" details to prevent conflicts with serving calendar files

--- rdrhc_calendar/urls.py
+++ rdrhc_calendar/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.calendar_index, name="calendar_index"),
+    url(r"^how-to$", views.calendar_index, name="calendar_index"),
 ]
