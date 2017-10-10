f8cb3f4693e18b1c448978bc105371ab5a919df5
studybuffalo <studybuffalo@gmail.com>
2017-10-09 20:23:31 -0600

    Adding missed slash

--- rdrhc_calendar/urls.py
+++ rdrhc_calendar/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^how-to$", views.calendar_index, name="calendar_index"),
+    url(r"^how-to/$", views.calendar_index, name="calendar_index"),
 ]
