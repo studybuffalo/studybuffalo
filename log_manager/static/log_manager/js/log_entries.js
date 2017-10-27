4ef305f3f904a06aee2a4738a72ce3534761212e
studybuffalo <studybuffalo@gmail.com>
2017-10-26 20:14:51 -0600

    Adding code to display all details of a log entry on click

--- log_manager/static/log_manager/js/log_entries.js
+++ log_manager/static/log_manager/js/log_entries.js
@@ -1,4 +1,4 @@
-function process_log_entries(results) {
+function processLogEntries(results) {
     // Remove all the previous entries first
     $(".log_entry").remove();
 
@@ -12,6 +12,7 @@ function process_log_entries(results) {
             .addClass("level_" + results[i]["level_no"])
             .addClass("log_entry")
             .attr("data-id", results[i]["entry_id"])
+            .on("click", function () { viewDetails(this); })
             .appendTo($logDiv);
 
         // Create the elements for asc_time
@@ -96,10 +97,266 @@ function retrieveEntries() {
         type: "GET",
         dataType: "json",
         success: function (results) {
-            process_log_entries(results);
+            processLogEntries(results);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.error("Error retrieving updated log entries");
+            console.error(textStatus + ": " + errorThrown);
+        }
+    });
+}
+
+/****************************************************************************
+ *	closeInfoPopup()	Closes the popup window generated to dispaly drug 	*
+ *						info												*
+ ****************************************************************************/
+function closePopup(e) {
+    let info_popup = $("#log_entry_info")[0]
+    
+    if (e !== info_popup) {
+        $("#popup").remove();
+    }
+}
+
+function displayDetails(entry) {
+    // Collect page, viewport, and element dimensions/coordinates
+    const pageHt = $(document).height();
+    const pageWid = $(document).width();
+    const screenWid = $(window).width();
+    const scrollHt = document.body.scrollTop ||
+        document.documentElement.scrollTop;
+
+    // Create a div that covers the page to catch all outside clicks
+    let $coverDiv = $("<div></div>");
+    $coverDiv
+        .attr("id", "popup")
+        .height(pageHt)
+        .width(pageWid)
+        .on("click", function (e) { closePopup(this); })
+        .prependTo("body");
+    
+    // Calculate pop-up width (90% of the window)
+    let popupWid = screenWid * 0.9;
+
+    // Give the popup a 5% margin on both sides
+    let popupLeft = screenWid * 0.05;
+
+    // Place the popup at the top of the viewport
+    let popupTop = scrollHt + 10;
+
+    // Popup div positions to left of trigger button
+    let $infoDiv = $("<div></div>");
+    $infoDiv
+        .attr("id", "log_entry_info")
+        .css({
+            "left": popupLeft + "px",
+            "top": popupTop  + "px",
+            "width": popupWid + "px"
+        })
+        .appendTo($coverDiv);
+
+    // The Pop-Up Div Title
+    let $titleDiv = $("<div></div>");
+    $titleDiv
+        .appendTo($infoDiv);
+
+    let $title = $("<h2></h2>");
+    $title
+        .text("Log Entry Details")
+        .appendTo($titleDiv);
+
+    // The app_name div
+    let $appNameDiv = $("<div></div>");
+    $appNameDiv
+        .appendTo($infoDiv);
+
+    let $appNameStrong = $("<strong></strong>");
+    $appNameStrong
+        .text("Application Name: ")
+        .appendTo($appNameDiv);
+
+    let $appNameSpan = $("<span></span>");
+    $appNameSpan
+        .text(entry.app_name)
+        .appendTo($appNameDiv);
+
+    // The asc_time and msec div
+    let $timeDiv = $("<div></div>");
+    $timeDiv
+        .appendTo($infoDiv);
+
+    let $timeStrong = $("<strong></strong>");
+    $timeStrong
+        .text("Log Entry Time: ")
+        .appendTo($timeDiv);
+
+    let $timeSpan = $("<span></span>");
+    $timeSpan
+        .text(entry.asc_time + "." + entry.msec + " " + entry.timezone)
+        .appendTo($timeDiv);
+
+    // The created div
+    let createdText = entry.created + " seconds";
+    createdText += entry.relative_created ? " (" + entry.relative_created
+        + " milliseconds since logging module loaded)" : "";
+
+    let $createdDiv = $("<div></div>");
+    $createdDiv
+        .appendTo($infoDiv);
+
+    let $createdStrong = $("<strong></strong>");
+    $createdStrong
+        .text("Entry Created: ")
+        .appendTo($createdDiv);
+    
+    let $createdSpan = $("<span></span>");
+    $createdSpan
+        .text(createdText)
+        .appendTo($createdDiv);
+
+    // The level_name and level_no div
+    const logLevelText = entry.level_no + " (" + entry.level_name + ")";
+
+    let $logLevelDiv = $("<div></div>");
+    $logLevelDiv
+        .appendTo($infoDiv);
+
+    let $logLevelStrong = $("<strong></strong>");
+    $logLevelStrong
+        .text("Log Level: ")
+        .appendTo($logLevelDiv);
+
+    let $logLevelSpan = $("<span></span>");
+    $logLevelSpan
+        .text(logLevelText)
+        .appendTo($logLevelDiv);
+
+    // The message div
+    let $messageDiv = $("<div></div>");
+    $messageDiv
+        .appendTo($infoDiv);
+
+    let $messageStrong = $("<strong></strong>");
+    $messageStrong
+        .text("Message: ")
+        .appendTo($messageDiv);
+
+    let $messageSpan = $("<span></span>");
+    $messageSpan
+        .text(entry.message)
+        .appendTo($messageDiv);
+
+    // The path_name, file_name, name, module, func_name, and line_no div
+    const logPathText = entry.path_name + " > " + entry.file_name
+        + " > " + entry.name + " > " + entry.module + " > " + entry.func_name
+        + " > line " + entry.line_no;
+
+    let $logPathDiv = $("<div></div>");
+    $logPathDiv
+        .appendTo($infoDiv);
+
+    let $logPathStrong = $("<strong></strong>");
+    $logPathStrong
+        .text("Log Path: ")
+        .appendTo($logPathDiv);
+    
+    let $logPathSpan = $("<span></span>");
+    $logPathSpan
+        .text(logPathText)
+        .appendTo($logPathDiv);
+
+    // The exc_info div
+    let excInfoText = entry.exc_info;
+
+    let $excInfoDiv = $("<div></div>");
+    $excInfoDiv
+        .appendTo($infoDiv);
+
+    let $excInfoStrong = $("<strong></strong>");
+    $excInfoStrong
+        .html("Exception Info: ")
+        .appendTo($excInfoDiv);
+
+    let $excInfoPre = $("<pre></pre>");
+    $excInfoPre
+        .text(entry.exc_info)
+        .appendTo($excInfoDiv);
+
+    // The process and process_name div
+    const processText = entry.process + " (" + entry.process_name + ")";
+
+    let $processDiv = $("<div></div>");
+    $processDiv
+        .appendTo($infoDiv);
+
+    let $processStrong = $("<strong></strong>");
+    $processStrong
+        .text("Process: ")
+        .appendTo($processDiv);
+
+    let $processSpan = $("<span></span>");
+    $processSpan
+        .text(processText)
+        .appendTo($processDiv);
+
+    // The thread and thread_name div
+    const threadText = entry.thread + " (" + entry.thread_name + ")";
+
+    let $threadDiv = $("<div></div>");
+    $threadDiv
+        .appendTo($infoDiv);
+
+    let $threadStrong = $("<strong></strong>");
+    $threadStrong
+        .text("Thread: ")
+        .appendTo($threadDiv);
+
+    let $threadSpan = $("<span></span>");
+    $threadSpan
+        .text(threadText)
+        .appendTo($threadDiv);
+
+    // The stack_info div
+    let $stackDiv = $("<div></div>");
+    $stackDiv
+        .appendTo($infoDiv);
+
+    let $stackStrong = $("<strong></strong>");
+    $stackStrong
+        .text("Stack Info: ")
+        .appendTo($stackDiv);
+
+    let $stackSpan = $("<span></span>");
+    $stackSpan
+        .text(entry.stack_info)
+        .appendTo($stackDiv);
+
+    // Close Button
+    let $closeDiv = $("<div></div>");
+    $closeDiv
+        .attr("class", "close")
+        .appendTo($infoDiv);
+
+    let $closeButton = $("<input type='button'>")
+    $closeButton
+        .val("Close")
+        .on("click", function () { closePopup(); })
+        .appendTo($closeDiv);
+}
+
+function viewDetails(a) {
+    entry_id = $(a).attr("data-id");
+    
+    $.ajax({
+        url: "retrieve-entry/",
+        data: {entry_id: entry_id},
+        type: "GET",
+        dataType: "json",
+        success: function (results) {
+            displayDetails(results);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error retrieving desired log entry");
             console.error(textStatus + ": " + errorThrown);
         }
     });
--- log_manager/views.py
+++ log_manager/views.py
@@ -62,99 +62,6 @@ def log_entries(request):
     )
 
 @permission_required("log_manager.can_view", login_url="/accounts/login/")
-def update_entries(request):
-    # Return query data as JSON
-    if request.GET:
-        # Assemble the app name list
-        if request.GET["app_names"]:
-            app_names_strings = request.GET["app_names"].split(",")
-
-            # Convert the app name codes into integers
-            app_names = []
-
-            for app in app_names_strings:
-                app_names.append(int(app))
-        else:
-            app_names = []
-        
-        
-        # Assemble the log level list
-        if request.GET["log_levels"]:
-            log_levels_string = request.GET["log_levels"].split(",")
-
-            # Conver the log levels into integers
-            log_levels = []
-
-            for level in log_levels_string:
-                log_levels.append(int(level))
-        else:
-            log_levels = []
-
-        # Format the datetime values
-        if request.GET["start_date"]:
-            start_date_string = request.GET["start_date"]
-
-            # Try to format it into a datetime object, otherwise use default
-            try:
-                start_date_naive = datetime.strptime(
-                    start_date_string,
-                    "%Y-%m-%d %H:%M:%S"
-                )
-
-                start_date = pytz.timezone("UTC").localize(start_date_naive)
-            except Exception:
-                start_date = timezone.now() - timedelta(months=144)
-        else:
-            start_date = timezone.now() - timedelta(months=144)
-
-        if request.GET["start_date"]:
-            end_date_string = request.GET["end_date"]
-
-            # Try to format it into a datetime object, otherwise use default
-            try:
-                end_date_naive = datetime.strptime(
-                    end_date_string,
-                    "%Y-%m-%d %H:%M:%S"
-                )
-
-                end_date = pytz.timezone("UTC").localize(end_date_naive)
-            except Exception:
-                end_date = timezone.now()
-        else:
-            end_date = timezone.now()
-        
-    # Create a queryset object that matches all the required criteria
-    log_entries = LogEntry.objects.filter(
-        app_name__in=app_names
-    ).filter(
-        level_no__in=log_levels
-    ).filter(
-        asc_time__gte=start_date
-    ).filter(
-        asc_time__lte=end_date
-    ).order_by(
-        "-asc_time"
-    )
-
-    # Format the log_entries as a json dictionary
-    response = []
-
-    for entry in log_entries:
-        response.append({
-            "entry_id": entry.id,
-            "asc_time": entry.asc_time.strftime("%Y-%m-%d %H:%M:%S %Z"),
-            "app_name": entry.app_name.name,
-            "func_name": entry.func_name,
-            "message": entry.message,
-            "level_no": entry.level_no
-        })
-
-    return HttpResponse(
-        json.dumps(response, cls=DjangoJSONEncoder), 
-        content_type="application/json"
-    )
-
-@permission_required("log_manager.can_view", login_url="/accounts/login/")
 def app_add(request):
     # If this is a POST request then process the Form data
     if request.method == "POST":
@@ -327,4 +234,139 @@ def log_entries_delete(request, id):
         request, 
         "log_manager/log_entries_delete.html", 
         {"app": app_instance.name}
+    )
+
+@permission_required("log_manager.can_view", login_url="/accounts/login/")
+def update_entries(request):
+    # Return query data as JSON
+    if request.GET:
+        # Assemble the app name list
+        if request.GET["app_names"]:
+            app_names_strings = request.GET["app_names"].split(",")
+
+            # Convert the app name codes into integers
+            app_names = []
+
+            for app in app_names_strings:
+                app_names.append(int(app))
+        else:
+            app_names = []
+        
+        
+        # Assemble the log level list
+        if request.GET["log_levels"]:
+            log_levels_string = request.GET["log_levels"].split(",")
+
+            # Conver the log levels into integers
+            log_levels = []
+
+            for level in log_levels_string:
+                log_levels.append(int(level))
+        else:
+            log_levels = []
+
+        # Format the datetime values
+        if request.GET["start_date"]:
+            start_date_string = request.GET["start_date"]
+
+            # Try to format it into a datetime object, otherwise use default
+            try:
+                start_date_naive = datetime.strptime(
+                    start_date_string,
+                    "%Y-%m-%d %H:%M:%S"
+                )
+
+                start_date = pytz.timezone("UTC").localize(start_date_naive)
+            except Exception:
+                start_date = timezone.now() - timedelta(months=144)
+        else:
+            start_date = timezone.now() - timedelta(months=144)
+
+        if request.GET["start_date"]:
+            end_date_string = request.GET["end_date"]
+
+            # Try to format it into a datetime object, otherwise use default
+            try:
+                end_date_naive = datetime.strptime(
+                    end_date_string,
+                    "%Y-%m-%d %H:%M:%S"
+                )
+
+                end_date = pytz.timezone("UTC").localize(end_date_naive)
+            except Exception:
+                end_date = timezone.now()
+        else:
+            end_date = timezone.now()
+        
+    # Create a queryset object that matches all the required criteria
+    log_entries = LogEntry.objects.filter(
+        app_name__in=app_names
+    ).filter(
+        level_no__in=log_levels
+    ).filter(
+        asc_time__gte=start_date
+    ).filter(
+        asc_time__lte=end_date
+    ).order_by(
+        "-asc_time"
+    )
+
+    # Format the log_entries as a json dictionary
+    response = []
+
+    for entry in log_entries:
+        response.append({
+            "entry_id": entry.id,
+            "asc_time": entry.asc_time.strftime("%Y-%m-%d %H:%M:%S %Z"),
+            "app_name": entry.app_name.name,
+            "func_name": entry.func_name,
+            "message": entry.message,
+            "level_no": entry.level_no
+        })
+
+    return HttpResponse(
+        json.dumps(response, cls=DjangoJSONEncoder), 
+        content_type="application/json"
+    )
+
+@permission_required("log_manager.can_view", login_url="/accounts/login/")
+def retrieve_entry(request):
+    # Return query data as JSON
+    if request.GET:
+        # Assemble the app name list
+        entry_id = request.GET["entry_id"] if request.GET["entry_id"] else None
+
+        if entry_id:
+            entry = LogEntry.objects.filter(id=entry_id).get()
+
+            # Format the entry as a json dictionary
+            response = {
+                "app_name": entry.app_name.name,
+                "asc_time": entry.asc_time.strftime("%Y-%m-%d %H:%M:%S"),
+                "timezone": entry.asc_time.strftime("%Z"),
+                "created": entry.created,
+                "exc_info": entry.exc_info,
+                "file_name": entry.file_name,
+                "func_name": entry.func_name,
+                "level_name": entry.level_name,
+                "level_no": entry.level_no,
+                "line_no": entry.line_no,
+                "message": entry.message,
+                "module": entry.module,
+                "msec": entry.msecs,
+                "name": entry.name,
+                "path_name": entry.path_name,
+                "process": entry.process,
+                "process_name": entry.process_name,
+                "relative_created": entry.relative_created,
+                "stack_info": entry.stack_info,
+                "thread": entry.thread,
+                "thread_name": entry.thread_name
+            }
+        else:
+            response = {}
+   
+    return HttpResponse(
+        json.dumps(response, cls=DjangoJSONEncoder), 
+        content_type="application/json"
     )
