0f485e849defa21bab23b40023f8bb84f15b5699
studybuffalo <studybuffalo@gmail.com>
2017-11-19 21:02:58 -0700

    Adding initial app models and fields, constructing admin page to control values

--- substitutions/admin.py
+++ substitutions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Apps, ModelFields
 
-# Register your models here.
+class FieldsInline(admin.TabularInline):
+    model = ModelFields
+
+    extra = 2
+    verbose_name = "Model Field"
+    verbose_name_plural = "Model Fields"
+
+@admin.register(Apps)
+class AppsAdmin(admin.ModelAdmin):
+    list_display = ("app_name", "model_pending", "model_sub")
+    ordering = ("app_name", "model_pending", "model_sub")
+    fields = ["app_name", ("model_pending", "model_sub")]
+    inlines = [FieldsInline]
--- substitutions/models.py
+++ substitutions/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Apps(models.Model):
+    """List of all Django applications to monitor"""
+    app_name = models.CharField(
+        help_text="The name of the Django App to include",
+        max_length=50,
+    )
+    model_pending = models.CharField(
+        help_text="The model name containing the pending substitutions",
+        max_length=50,
+    )
+    model_sub = models.CharField(
+        help_text=(
+            "The model name where the verified substitutions should "
+            "be added to"
+        ),
+        max_length=50,
+    )
+
+    class Meta:
+        verbose_name = "Application"
+        verbose_name_plural = "Applications"
+
+    def __str__(self):
+        return self.app_name
+
+
+class ModelFields(models.Model):
+    """List of the model fields to include"""
+    app = models.ForeignKey(
+        to="Apps",
+        on_delete=models.CASCADE
+    )
+    field_name = models.CharField(
+        help_text="A field name from the model to include",
+        max_length=50,
+    )
+    dictionary_check = models.BooleanField(
+        default=False,
+        help_text="Whether to add a online search button to confirm spelling",
+    )
+
+    class meta:
+        verbose_name = "Model Field"
+        verbose_name_plural = "Model Fields"
+
+    def __str__(self):
+        return "{} - {}".format(self.app, self.field_name)
