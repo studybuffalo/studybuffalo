9e3b594062c25062bb6f62b48b687d60b44ca4fc
studybuffalo <studybuffalo@gmail.com>
2017-11-19 19:33:22 -0700

    Adding admin view for the AHFS Sub Pend model

--- hc_dpd/admin.py
+++ hc_dpd/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import SubAHFS
+from .models import SubAHFS, SubAHFSPend
 
 @admin.register(SubAHFS)
 class SubAHFSAdmin(admin.ModelAdmin):
+    list_display = ("original", "substitution")
+    ordering = ("original",)
+
+@admin.register(SubAHFSPend)
+class SubAHFSPendAdmin(admin.ModelAdmin):
     list_display = ("original", "substitution")
     ordering = ("original",)
