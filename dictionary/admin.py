4b75fc3fdc6770c66a49bf5d3f670e3812b363b9
studybuffalo <studybuffalo@gmail.com>
2017-12-02 12:13:36 -0700

    Removing the monitored application/model/fields from the dictionary app - this will be moved into the sb_dictionary_tool

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,33 +1,18 @@
 from django.contrib import admin
 from .models import (
-    MonitoredApplication, MonitoredModel, MonitoredField, DictionaryType, 
-    Language, Word, ExcludedWord
+    Language, DictionaryType, DictionaryClass, Word, ExcludedWord
 )
 
-class MonitoredModelInline(admin.TabularInline):
-    model = MonitoredModel
-
-class MonitoredFieldInline(admin.TabularInline):
-    model = MonitoredField
-
-@admin.register(MonitoredApplication)
-class MonitoredApplicationAdmin(admin.ModelAdmin):
-    inlines = [
-        MonitoredModelInline,
-    ]
-
-@admin.register(MonitoredModel)
-class MonitoredModelAdmin(admin.ModelAdmin):
-    inlines = [
-        MonitoredFieldInline,
-    ]
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(DictionaryType)
 class DictionaryTypeAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Language)
-class LanguageAdmin(admin.ModelAdmin):
+@admin.register(DictionaryClass)
+class DictionaryClassAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Word)
--- dictionary/models.py
+++ dictionary/models.py
@@ -21,48 +21,6 @@ class DictionaryClass(models.Model):
     def __str__(self):
         return self.class_name
 
-class MonitoredApplication(models.Model):
-    """Name of application to be monitored for dictionary items"""
-    application_name = models.CharField(max_length=50,)
-
-    class Meta:
-        permissions = (
-            ("can_view", "Can view the dictionary review application"),
-        )
-
-    def __str__(self):
-        return self.application_name
-
-class MonitoredModel(models.Model):
-    """Name of a model to be included from an application"""
-    monitored_application = models.ForeignKey(
-        to="MonitoredApplication",
-        on_delete=models.CASCADE,
-    )
-    model_name = models.CharField(max_length=50,)
-
-    def __str__(self):
-        return self.model_name
-
-class MonitoredField(models.Model):
-    """Name of a field to be included from a model"""
-    monitored_model = models.ForeignKey(
-        to="MonitoredModel",
-        on_delete=models.CASCADE,
-    )
-    dictionary_type = models.ForeignKey(
-        to="DictionaryType",
-        on_delete=models.CASCADE,
-    )
-    language = models.ForeignKey(
-        to="Language",
-        on_delete=models.CASCADE,
-    )
-    field_name = models.CharField(max_length=50,)
-    
-    def __str__(self):
-        return self.field_name
-
 class Word(models.Model):
     """A single word in the dictionary"""
     dictionary_type = models.ForeignKey(
@@ -109,6 +67,9 @@ class WordPending(models.Model):
     word = models.CharField(max_length=50,)
     
     class Meta:
+        permissions = (
+            ("can_view", "Can view the dictionary review application"),
+        )
         verbose_name = "Word (Pending)"
         verbose_name_plural = "Words (Pending)"
 
