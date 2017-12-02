cae1c01100f91d115e8f605fcc43d1854a3cae5a
studybuffalo <studybuffalo@gmail.com>
2017-12-02 12:09:36 -0700

    Adding a dictionary class model to specify dictionaries for humans, animals, etc.

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Language(models.Model):
+    """Language of the word"""
+    language = models.CharField(max_length=25,)
+    
+    def __str__(self):
+        return self.language
+
 class DictionaryType(models.Model):
     """Dictionary type for creation of specific dictionary categories"""
     dictionary_name = models.CharField(max_length=50,)
@@ -7,12 +14,12 @@ class DictionaryType(models.Model):
     def __str__(self):
         return self.dictionary_name
 
-class Language(models.Model):
-    """Language of the word"""
-    language = models.CharField(max_length=25,)
-    
+class DictionaryClass(models.Model):
+    """Specifies a dictionary classification"""
+    class_name = models.CharField(max_length=20,)
+
     def __str__(self):
-        return self.language
+        return self.class_name
 
 class MonitoredApplication(models.Model):
     """Name of application to be monitored for dictionary items"""
@@ -68,6 +75,11 @@ class Word(models.Model):
         on_delete=models.SET_NULL,
         null=True,
     )
+    dictionary_class = models.ForeignKey(
+        to=DictionaryClass,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     word = models.CharField(max_length=50,)
     
     def __str__(self):
@@ -75,14 +87,23 @@ class Word(models.Model):
 
 class WordPending(models.Model):
     """A single word pending inclusion into Word"""
+    language = models.ForeignKey(
+        to="Language",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     dictionary_type = models.ForeignKey(
         to="DictionaryType",
         on_delete=models.SET_NULL,
         null=True,
     )
-    language = models.ForeignKey(
-        to="Language",
+    dictionary_class = models.ForeignKey(
+        to=DictionaryClass,
         on_delete=models.SET_NULL,
+        null=True,
+    )
+    original_words = models.CharField(
+        max_length=300,
         null=True,
     )
     word = models.CharField(max_length=50,)
@@ -103,6 +124,11 @@ class ExcludedWord(models.Model):
     )
     language = models.ForeignKey(
         to="Language",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    dictionary_class = models.ForeignKey(
+        to=DictionaryClass,
         on_delete=models.SET_NULL,
         null=True,
     )
