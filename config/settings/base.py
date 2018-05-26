8b6cc0a3bd77665e6cc6ca6b24ab3b8dbdd543c5
studybuffalo <studybuffalo@gmail.com>
2018-05-26 07:50:01 -0600

    Changing to email registration

--- config/settings/base.py
+++ config/settings/base.py
@@ -234,7 +234,7 @@ MANAGERS = ADMINS
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
