46383923eba3e6b41df681f1f3561fdd17d16021
studybuffalo <studybuffalo@gmail.com>
2018-05-27 07:39:27 -0600

    Correcting small errors in models

--- study_buffalo/flash_cards/models.py
+++ study_buffalo/flash_cards/models.py
@@ -12,7 +12,7 @@ class PartContainer(models.Model):
 class AbstractPart(models.Model):
     '''Abstract model to construct parts'''
     container = models.ForeignKey(
-        to=PartContainer,
+        PartContainer,
         on_delete=models.CASCADE,
     )
     order = models.PositiveIntegerField(
@@ -24,9 +24,10 @@ class AbstractPart(models.Model):
         abstract = True
 
 class TextPart(AbstractPart):
-    text = models.CharField(
-        max_length=2048,
+    text = models.TextField(
+        max_length=2000,
     )
+    history = HistoricalRecords()
 
 class MediaPart(AbstractPart):
     MEDIA_TYPES = (
@@ -34,21 +35,23 @@ class MediaPart(AbstractPart):
         ('a', 'Audio'),
         ('v', 'Video'),
     )
+
     media_type = models.CharField(
         max_length=1,
         choices=MEDIA_TYPES,
         default='i',
     )
     media = models.FileField(
-        upload_to='flash_cards',
+        upload_to='flash_cards/',
     )
+    history = HistoricalRecords()
 
 class MultipleChoiceContainer(models.Model):
     '''Container for multiple choice answers'''
     history = HistoricalRecords()
 
 class MultipleChoiceAnswer(models.Model):
-    question_container = models.ForeignKey(
+    container = models.ForeignKey(
         MultipleChoiceContainer,
         on_delete=models.CASCADE,
     )
@@ -102,26 +105,29 @@ class Card(models.Model):
     question = models.ForeignKey(
         PartContainer,
         on_delete=models.CASCADE,
-        null=True,
     ),
     answer_multiple_choice = models.ForeignKey(
         MultipleChoiceContainer,
         on_delete=models.CASCADE,
+        blank=True,
         null=True,
     )
     answer_matching = models.ForeignKey(
         MatchingAnswer,
         on_delete=models.CASCADE,
+        blank=True,
         null=True,
     )
     answer_freeform = models.ForeignKey(
         PartContainer,
         on_delete=models.CASCADE,
+        blank=True,
         null=True,
     )
     rationale = models.ForeignKey(
         PartContainer,
         on_delete=models.SET_NULL,
+        blank=True,
         null=True,
     )
     reviewed = models.BooleanField(
@@ -143,33 +149,14 @@ class Reference(models.Model):
         Card,
         on_delete=models.CASCADE,
     )
-    reference = models.CharField(
-        max_length=256,
-    )
-    history = HistoricalRecords()
-
-class Tag(models.Model):
-    tag_name = models.CharField(
-        max_length=128,
-    )
-    card = models.ForeignKey(
-        Card,
-        on_delete=models.CASCADE,
-    )
-
-class Synonym(models.Model):
-    tag = models.ForeignKey(
-        Tag,
-        on_delete=models.CASCADE,
-    )
-    tag_name = models.CharField(
-        max_length=128,
+    reference = models.TextField(
+        max_length=500,
     )
     history = HistoricalRecords()
 
 class Deck(models.Model):
     deck_name = models.CharField(
-        max_length=128,
+        max_length=255,
     )
     reviewed = models.BooleanField(
         default=False,
@@ -185,18 +172,47 @@ class Deck(models.Model):
     )
     history = HistoricalRecords()
 
-class CardSet(models.Model):
+class CardDeck(models.Model):
     deck = models.ForeignKey(
         Deck,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        null=True,
     )
     card = model.ForeignKey(
         Card,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    history = HistoricalRecords()
+
+class Tag(models.Model):
+    tag_name = models.CharField(
+        max_length=100,
+    )
+    history = HistoricalRecords()
+
+class Synonym(models.Model):
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+    )
+    synonym_name = models.CharField(
+        max_length=100,
+    )
+    history = HistoricalRecords()
+
+class CardTag(models.Model):
+    card = models.ForeignKey(
+        Card,
+        on_delete=models.CASCADE
+    )
+    tag = models.ForeignKey(
+        Tag,
         on_delete=models.CASCADE,
     )
     history = HistoricalRecords()
 
-class TagSet(models.Model):
+class TagDeck(models.Model):
     deck = models.ForeignKey(
         Deck,
         on_delete=models.CASCADE,
@@ -258,14 +274,15 @@ class UserStats(models.Model):
 class Feedback(models.Model):
     user = models.ForeignKey(
         get_user_model,
+        blank=True,
         null=True,
         default=None,
     )
     date_submitted = models.DateField(
         default=timezone.now
     )
-    comment = models.CharField(
-        max_length = 1024,
+    comment = models.TextField(
+        max_length = 2000,
     )
     history = HistoricalRecords()
 
@@ -283,67 +300,3 @@ class DeckFeedback(Feedback):
         Deck,
         on_delete=models.CASCADE
     )
-
-'''Other Stats that can be compiled from models
-    Total number questions user has completed and breakdown (from sets model)
-    Number of created/modified cards and sets from history
-    Current status on deck from the last entry on set
-'''
-# # Study Buffalo Flash Cards
-# ## Overview
-# - Users will be presented with flash cards for them to answer in various formats
-# - Formats may include multiple choice, fill in the blank, free-form
-# - Each individual card could be tagged into multiple collections to generate unique sets
-# - Cards and sets can be created by users; users can report any concerns with questions
-# - Certain individuals will have the ability to verify cards and resolve concerns (receive fee for this?)
-# - Ability will be to have some features monetized (e.g. Mental Health & Movember)
-# - Let users set the number of questions they answer
-# - Recycle older questions if the user incorrectly answers
-# - Leaderboards, personal records?
-# - Vast majority of all features will require proper records of changes
-
-# ## Flash Card Design
-# - Questions and answers
-# 	- Text
-# 	- Audio
-# 	- Video
-# 	- Images
-
-# ## Question Types
-# ### Multiple Choice
-# - One question
-# - Multiple choices, one answer
-# - Can be marked either correct or incorrect
-
-# ### Multiple Multiple Choice
-# - One question
-# - Multiple choices, multiple answers
-# - Can be marked either correct, partially correct, or incorrect
-
-# ### Matching
-# - Multiple questions
-# - Multiple answers
-# - Can be marked either correct, partially correct, or incorrect
-
-# ### Freeform
-# - One question
-# - No answers
-# - Marked by user as either correct, partially correct, or incorrect
-
-# ## Cards & Sets
-# - Each card has one question type
-# - Each card may have multiple "tags"
-# - Sets can be made up of specific cards and/or tags
-
-# ## Tags
-# - Can be created by the users
-# - Tags can have synonyms that map to a single tag
-# - Advanced users may merge and consolidate tags
-
-# ## App Functionality for User
-# - Users will be required to create an account to continue
-# - Users will have ability to study a set, create new cards, and create new sets
-# - Users may combine multiple sets to study
-# - Users will set number of questions to study in the set
-# - Users may set a time limit to the session
-# - Study session ends with timer is up or all questions are answered
---
+++ study_buffalo/flash_cards/views.py
@@ -0,0 +1,63 @@
+# # Study Buffalo Flash Cards
+# ## Overview
+# - Users will be presented with flash cards for them to answer in various formats
+# - Formats may include multiple choice, fill in the blank, free-form
+# - Each individual card could be tagged into multiple collections to generate unique sets
+# - Cards and sets can be created by users; users can report any concerns with questions
+# - Certain individuals will have the ability to verify cards and resolve concerns (receive fee for this?)
+# - Ability will be to have some features monetized (e.g. Mental Health & Movember)
+# - Let users set the number of questions they answer
+# - Recycle older questions if the user incorrectly answers
+# - Leaderboards, personal records?
+# - Vast majority of all features will require proper records of changes
+
+# ## Flash Card Design
+# - Questions and answers
+# 	- Text
+# 	- Audio
+# 	- Video
+# 	- Images
+
+# ## Question Types
+# ### Multiple Choice
+# - One question
+# - Multiple choices, one answer
+# - Can be marked either correct or incorrect
+
+# ### Multiple Multiple Choice
+# - One question
+# - Multiple choices, multiple answers
+# - Can be marked either correct, partially correct, or incorrect
+
+# ### Matching
+# - Multiple questions
+# - Multiple answers
+# - Can be marked either correct, partially correct, or incorrect
+
+# ### Freeform
+# - One question
+# - No answers
+# - Marked by user as either correct, partially correct, or incorrect
+
+# ## Cards & Sets
+# - Each card has one question type
+# - Each card may have multiple "tags"
+# - Sets can be made up of specific cards and/or tags
+
+# ## Tags
+# - Can be created by the users
+# - Tags can have synonyms that map to a single tag
+# - Advanced users may merge and consolidate tags
+
+# ## App Functionality for User
+# - Users will be required to create an account to continue
+# - Users will have ability to study a set, create new cards, and create new sets
+# - Users may combine multiple sets to study
+# - Users will set number of questions to study in the set
+# - Users may set a time limit to the session
+# - Study session ends with timer is up or all questions are answered
+
+# Other Stats that can be compiled from models
+#     Total number questions user has completed and breakdown (from sets model)
+#     Number of created/modified cards and sets from history
+#     Current status on deck from the last entry on set
