5d9bcbd0501460a3dfb0f7b3eae598fd4f18458e
studybuffalo <studybuffalo@gmail.com>
2018-05-29 13:15:40 -0600

    Updating the API urls to allow versioning

--- study_buffalo/flash_cards/urls.py
+++ study_buffalo/flash_cards/urls.py
@@ -1,14 +1,16 @@
+from django.conf.urls import include
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from flash_cards import views
 
 urlpatterns = [
-    path('cards/', views.Cards.as_view(), name='card-list'),
-    path('decks/', views.DeckList.as_view(), name='deck-list'),
-    path('decks/<uuid:deck_uuid>/', views.DeckDetail.as_view(), name='deck-detail'),
-    path('tags/', views.tags, name='tag-list'),
-    path('', views.api_root, name='root'),
+    path('api/authentication/', include('rest_framework.urls')),
+    path('api/v1/cards/', views.Cards.as_view(), name='card-list'),
+    path('api/v1/decks/', views.DeckList.as_view(), name='deck-list'),
+    path('api/v1/decks/<uuid:deck_uuid>/', views.DeckDetail.as_view(), name='deck-detail'),
+    path('api/v1/tags/', views.tags, name='tag-list'),
+    path('api/v1/', views.api_root, name='root'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
