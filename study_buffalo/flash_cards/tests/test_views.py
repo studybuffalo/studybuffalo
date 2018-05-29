8e9ccc38cbba3559aa077c301157bae122765e0e
studybuffalo <studybuffalo@gmail.com>
2018-05-29 13:43:27 -0600

    Adding initial test for views

--- study_buffalo/flash_cards/tests/test_views.py
+++ study_buffalo/flash_cards/tests/test_views.py
@@ -1,7 +1,24 @@
 from django.urls import reverse
-
 from django.test import TestCase
+
+from .utils import create_user
 
 class APIRootTest(TestCase):
     def setUp(self):
-        pass
+        self.user = create_user()
+
+    def test_v1_root_url_exists(self):
+        '''Checks that the API root uses the correct URL'''
+        self.client.login(username=self.user.username, password='abcd123456')
+        response = self.client.get('/flash-cards/api/v1/')
+
+        # Check that page is accessible
+        self.assertEqual(response.status_code, 200)
+
+    def test_v1_root_accessible_by_name(self):
+        '''Checks that the dashboard URL name works properly'''
+        self.client.login(username=self.user.username, password='abcd123456')
+        response = self.client.get(reverse("flash_cards:api_v1:root"))
+
+        # Check that page is accessible
+        self.assertEqual(response.status_code, 200)
