5ff24c17c24b3bed33b514104e69c8e065a9e5b5
studybuffalo <studybuffalo@gmail.com>
2018-05-29 13:48:33 -0600

    interim changes

--- study_buffalo/flash_cards/urls.py
+++ study_buffalo/flash_cards/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include
 from django.urls import path
 
+app_name = 'flash_cards'
 urlpatterns = [
-    path('api/v1/', include('flash_cards.api.urls'), name='api_v1'),
+    path('api/v1/', include('flash_cards.api.urls', namespace='api_v1')),
 ]
