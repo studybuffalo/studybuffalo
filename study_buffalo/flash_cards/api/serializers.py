2f037eb99b7938b18136e3e982f31046e1a6e585
studybuffalo <studybuffalo@gmail.com>
2018-05-30 21:43:11 -0600

    Reconstructed Card serializer

--- study_buffalo/flash_cards/api/serializers.py
+++ study_buffalo/flash_cards/api/serializers.py
@@ -1,35 +1,32 @@
 # pylint: disable=abstract-method,arguments-differ
 from rest_framework import serializers
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
-
+from django.urls import reverse
 from flash_cards import models
 
 
 class SynonymSerializer(serializers.ModelSerializer):
-    tag = serializers.HyperlinkedRelatedField(
-        read_only=True,
-        view_name='flash_cards:api_v1:tag_detail',
-        lookup_field='tag_name',
-    )
-
     class Meta:
         model = models.Synonym
-        fields = ('synonym_name', 'tag', )
+        fields = ('synonym_name', )
 
-class TagSerializer(serializers.ModelSerializer):
-    synonyms = serializers.HyperlinkedRelatedField(
+class TagSerializer(serializers.Serializer):
+    tag_name = serializers.CharField(
+        max_length=100,
+    )
+    synonyms = SynonymSerializer(
         many=True,
-        read_only=True,
         required=False,
-        view_name='flash_cards:api_v1:synonym_detail',
-        lookup_field='synonym_name',
     )
 
     class Meta:
         model = models.Tag
         fields = ('tag_name', 'synonyms', )
 
+    def create(self, validated_data):
+        print("Test")
 
 class ReferenceSerializer(serializers.ModelSerializer):
     card = serializers.HyperlinkedRelatedField(
@@ -49,7 +46,7 @@ class DeckSerializer(serializers.ModelSerializer):
         model = models.Deck
         fields = (
             'id', 'deck_name', 'reviewed', 'active', 'date_modified',
-            'date_reviewed', 'tags'
+            'date_reviewed',
         )
 
 class QuestionPartSerializer(serializers.ModelSerializer):
@@ -57,26 +54,65 @@ class QuestionPartSerializer(serializers.ModelSerializer):
         model = models.QuestionPart
         fields = ('order', 'media_type', 'text', 'media')
 
+class MultipleChoiceAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.MultipleChoiceAnswer
+        fields = ('order', 'correct', )
+
+class MatchingAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.MatchingAnswer
+        fields = ('side', 'order', 'pair', )
+
+class FreeformAnswerPartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.FreeformAnswerPart
+        fields = ('order', 'media_type', 'text', 'media', )
+
+class RationalePartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.RationalePart
+        fields = ('order', 'media_type', 'text', 'media')
+
+class DeckForCardSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    url = serializers.SerializerMethodField(
+        source='get_url'
+    )
+
+    def get_url(self, data):
+        return self.context.get('request').build_absolute_uri(
+            reverse('flash_cards:api_v1:deck_detail', kwargs={'id': data.id})
+        )
+
+    def validate_id(self, data):
+        # Check that deck exists
+        try:
+            models.Deck.objects.get(id=data)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError(
+                'Provided deck does not exist: {}'.format(data)
+            )
+
+        return data
+
 class CardSerializer(serializers.ModelSerializer):
-    question_parts = QuestionPartSerializer(
-        many=True,
-    )
-    tags = TagSerializer(
-        many=True,
-    )
-    decks = DeckSerializer(
-        many=True,
-        required=False,
-    )
+    question_parts = QuestionPartSerializer(many=True)
+    multiple_choice_answers = MultipleChoiceAnswerSerializer(many=True, required=False)
+    matching_answers = MatchingAnswerSerializer(many=True, required=False)
+    freeform_answers = FreeformAnswerPartSerializer(many=True, required=False)
+    rationale_parts = RationalePartSerializer(many=True, required=False)
+    tags = TagSerializer(many=True)
+    decks = DeckForCardSerializer(many=True)
 
     class Meta:
         model = models.Card
         fields = (
-            'id', 'question_parts', 'reviewed', 'active', 'date_modified',
-            'date_reviewed', 'references', 'tags', 'decks'
+            'id', 'question_parts', 'multiple_choice_answers', 'matching_answers',
+            'freeform_answers', 'rationale_parts', 'reviewed', 'active',
+            'date_modified', 'date_reviewed', 'references', 'tags', 'decks'
         )
         depth = 2
-
 
     def get_or_create_tag(self, validated_data):
         tag_data = validated_data.pop('tags')
@@ -85,15 +121,26 @@ class CardSerializer(serializers.ModelSerializer):
 
         tags = []
 
+        # Check if this exists in the synonym list
         for tag in tag_data:
-            retrieved, created = models.Tag.objects.get_or_create(
-                tag_name=tag['tag_name']
-            )
+            synonym_list = models.Synonym.objects.filter(synonym_name=tag['tag_name'])
 
-            if retrieved:
-                tags.append(retrieved)
+            # Synonym exists, retrieve parent tag
+            if synonym_list.exists():
+
+                tags.append(synonym_list[0].tag)
+
+            # No synonym; create synonym and tag
             else:
-                tags.append(created)
+                created_tag = models.Tag.objects.create(
+                    tag_name=tag['tag_name'],
+                )
+                models.Synonym.objects.create(
+                    tag=created_tag,
+                    synonym_name=tag['tag_name'],
+                )
+
+                tags.append(created_tag)
 
         validated_data['tags'] = tags
 
@@ -102,22 +149,15 @@ class CardSerializer(serializers.ModelSerializer):
 
         # Extract the related models
         question_data = validated_data.pop('question_parts')
-        deck_data = validated_data.pop('decks')
         tags = validated_data.pop('tags')
-        print(deck_data)
+        decks = validated_data.pop('decks')
+
         # Create the card
         card = models.Card.objects.create(**validated_data)
 
         # Create the question
         for part in question_data:
             models.QuestionPart.objects.create(card=card, **part)
-
-        # Assign the card to the specified decks
-        for deck in deck_data:
-            models.CardDeck.objects.create(
-                card=card,
-                deck=models.Deck.objects.get(id=deck['id']),
-            )
 
         # Assign the tags to the card
         for tag in tags:
@@ -126,178 +166,12 @@ class CardSerializer(serializers.ModelSerializer):
                 tag=tag,
             )
 
+        # Assign card to the decks
+        for deck in decks:
+            print(deck)
+            models.CardDeck.objects.create(
+                card=card,
+                deck=models.Deck.objects.get(id=deck['id']),
+            )
+
         return card
-
-# class NewCardSerializer(serializers.Serializer):
-#     uuid = serializers.UUIDField(read_only=True)
-#     question = PartSerializer(many=True)
-#     answer = AnswerSerializer()
-#     rationale = PartSerializer(many=True)
-#     reviewed = serializers.BooleanField(default=False)
-#     active = serializers.BooleanField(default=False)
-#     date_modified = serializers.DateTimeField(default=timezone.now)
-#     date_reviewed = serializers.DateTimeField(default=None)
-#     decks = serializers.ListField(
-#         child=serializers.UUIDField(),
-#         required=False,
-#     )
-#     references = serializers.ListField(
-#         child=serializers.CharField(max_length=500),
-#         min_length=1,
-#     )
-#     tags = serializers.ListField(
-#         child=serializers.CharField(max_length=100),
-#         required=False,
-#     )
-
-#     def create(self, validated_data):
-#         def assemble_part_container(parts):
-#             container = PartContainer.objects.create()
-
-#             for part in parts:
-#                 if part['audio']:
-#                     MediaPart.objects.create(
-#                         container=container,
-#                         order=part['order'],
-#                         media=part['audio'],
-#                         media_type='a'
-#                     )
-#                 elif part['image']:
-#                     MediaPart.objects.create(
-#                         container=container,
-#                         order=part['order'],
-#                         media=part['image'],
-#                         media_type='i'
-#                     )
-#                 elif part['text']:
-#                     TextPart.objects.create(
-#                         container=container,
-#                         order=part['order'],
-#                         text=part['text']
-#                     )
-#                 elif part['video']:
-#                     MediaPart.objects.create(
-#                         container=container,
-#                         order=part['order'],
-#                         media=part['video'],
-#                         media_type='v'
-#                     )
-
-#             return container
-
-#         # Assemble the question
-#         question = assemble_part_container(validated_data['question'])
-
-#         # Create the answer
-#         if 'multiple_choice' in validated_data['answer']:
-#             answer_multiple_choice = None
-#             answer_matching = None
-#             answer_freeform = None
-#         elif 'matching' in validated_data['answer']:
-#             answer_matching = None
-#             answer_multiple_choice = None
-#             answer_freeform = None
-#         elif 'freeform' in validated_data['answer']:
-#             answer_freeform = assemble_part_container(validated_data['answer']['freeform'])
-#             answer_multiple_choice = None
-#             answer_matching = None
-
-#         # Create the rationale
-#         if validated_data['rationale']:
-#             rationale = assemble_part_container(validated_data['rationale'])
-
-#         # Create all the new models
-#         card = Card.objects.create(
-#             question=question,
-#             answer_multiple_choice=answer_multiple_choice,
-#             answer_matching=answer_matching,
-#             answer_freeform=answer_freeform,
-#             rationale=rationale,
-#         )
-
-#         # Add the card to any specified decks
-#         for deck in validated_data['decks']:
-#             CardDeck.objects.create(
-#                 deck=Deck.objects.get(deck_uuid=deck),
-#                 card=card
-#             )
-
-#         # Retrieve and create the tags
-#         for tag in validated_data['tags']:
-#             synonym = Synonym.objects.filter(synonym_name=tag)
-
-#             # Synonym found, match tag to card
-#             if synonym:
-#                 CardTag.objects.create(
-#                     card=card,
-#                     tag=synonym[0].tag,
-#                 )
-
-#             # New synonym/tag; create tag to match to card
-#             else:
-#                 new_tag = Tag.objects.create(
-#                     tag_name=tag
-#                 )
-#                 Synonym.objects.create(
-#                     tag=new_tag,
-#                     synonym_name=tag,
-#                 )
-#                 CardTag.objects.create(
-#                     card=card,
-#                     tag=new_tag,
-#                 )
-
-#         # Add any references for this card
-#         for reference in validated_data['references']:
-#             Reference.objects.create(
-#                 card=card,
-#                 reference=reference,
-#             )
-
-#         return validated_data
-
-#     def validate_question(self, data):
-#         if not data:
-#             raise serializers.ValidationError('A question is required.')
-
-#         return data
-
-#     def validate_decks(self, data):
-#         for deck in data:
-#             if Deck.objects.filter(deck_uuid=deck).exists() is False:
-#                 raise serializers.ValidationError('Specified deck does not exist.')
-
-#         return data
-
-# class PartContainerSerializer(serializers.ModelSerializer):
-#     text_parts = serializers.HyperlinkedRelatedField(
-#         many=True,
-#         read_only=True,
-#         view_name='flash_cards:api_v1:text_part_detail',
-#         lookup_field='part_uuid',
-#     )
-
-#     class Meta:
-#         model = PartContainer
-#         fields = ('text_parts',)
-
-# class TextPartSerializer(serializers.ModelSerializer):
-#     container = serializers.HyperlinkedRelatedField(
-#         read_only=True,
-#         view_name='flash_cards:api_v1:part_container_detail',
-#         lookup_field='container_uuid',
-#     )
-
-#     class Meta:
-#         model = TextPart
-#         fields = ('part_uuid', 'container', 'order', 'text')
-
-# class MultipleChoiceContainerSerializer(serializers.ModelSerializer):
-#     answers = serializers.HyperlinkedRelatedField(
-#         read_only=True,
-#         view_name='flash_cards:api_v1:multiple_choice_container_detail',
-#         lookup_field='container_uuid',
-#     )
-#     class Meta:
-#         model = MultipleChoiceContainer
-#         fields = ('container_uuid', 'answers')
