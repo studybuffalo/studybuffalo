c00d59d92e7534c267bb59d96abae1ca0e08d4e1
studybuffalo <studybuffalo@gmail.com>
2018-05-31 13:48:31 -0600

    Cleanign up URL patterns

--- study_buffalo/flash_cards/api/urls.py
+++ study_buffalo/flash_cards/api/urls.py
@@ -14,14 +14,6 @@ urlpatterns = [
     path('tags/', views.TagList.as_view(), name='tag_list'),
     path('tags/<str:tag_name>/', views.TagDetail.as_view(), name='tag_detail'),
     path('synonyms/<str:synonym_name>/', views.SynonymDetail.as_view(), name='synonym_detail'),
-    #path('references/<uuid:id>/', views.ReferenceDetail.as_view(), name='reference_detail'),
-    #path('part-containers/<uuid:container_uuid>/', views.PartContainerDetail.as_view(), name='part_container_detail'),
-    #path('text-parts/<uuid:part_uuid>/', views.TextPartDetail.as_view(), name='text_part_detail'),
-    #path(
-    # 'multiple-choice-containers/<uuid:container_uuid>/',
-    # views.MultipleChoiceContainerDetail.as_view(),
-    # name='multiple_choice_container_detail'
-    # ),
     path('', views.api_root, name='root'),
 ]
 
