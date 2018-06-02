5068b57c7ca94d9f7c47e4eb65e2a3ebbcb1ce09
Joshua Torrance <torrancj@gmail.com>
2018-06-02 12:16:14 -0600

    Testings out React functionality

--- study_buffalo/flash_cards/src/components/App.js
+++ study_buffalo/flash_cards/src/components/App.js
@@ -1,4 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Helloa, world!</h1>, document.getElementById('root'));
+function ListDisplay(prop) {
+  const items = prop.items
+  const listItems = items.map((item, index) =>
+    <li key={item} data-id={index}>{item}</li>
+  )
+
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+function App(prop) {
+  return (
+    <div>
+      <h1>Welcome {prop.name}</h1>
+      <ListDisplay items={[1,2,3]} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App name="Josh" />,
+  document.getElementById('root')
+);
