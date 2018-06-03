80d30deb62b5bf44bf870dfb856973928acab655
studybuffalo <studybuffalo@gmail.com>
2018-06-03 10:56:40 -0600

    Adding proper design to the menu and icon

--- study_buffalo/flash_cards/src/components/App.js
+++ study_buffalo/flash_cards/src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom'
 
 // TODO: Remove eslint rule for Link component when next ESLint edition released
+
 class Menu extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +13,7 @@ class Menu extends React.Component {
   }
 
   handleMenuClick(e) {
-    if (e.target.id==="menu-icon") {
+    if (e.target.id==="menu-icon" || e.target.parentElement.id==="menu-icon") {
       this.setState({isVisible: true});
     } else {
       this.setState({isVisible: false});
@@ -22,15 +23,30 @@ class Menu extends React.Component {
   render() {
     return (
       <div>
-        <button id="menu-icon" onClick={this.handleMenuClick}>Menu</button>
+        <button id="menu-icon" onClick={this.handleMenuClick}>
+          <span className="icon">&#8801;</span>
+          <span>Menu</span>
+        </button>
         <div id="side-menu" className={this.state.isVisible ? "visible" : ""}>
           <div id="menu-close-div">
             <button id="menu-close-button" onClick={this.handleMenuClick}>x</button>
           </div>
           <ul id="menu-items">
-            <li><Link to="/flash-cards/" onClick={this.handleMenuClick}>Dashboard</Link></li>
-            <li><Link to="/flash-cards/decks/" onClick={this.handleMenuClick}>Create &amp; Manage Decks</Link></li>
-            <li><Link to="/flash-cards/cards/" onClick={this.handleMenuClick}>Create &amp; Manage Cards</Link></li>
+            <li>
+              <NavLink exact to="/flash-cards/" onClick={this.handleMenuClick} activeClassName="selected">
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/flash-cards/decks/" onClick={this.handleMenuClick} activeClassName="selected">
+              Create &amp; Manage Decks
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/flash-cards/cards/" onClick={this.handleMenuClick} activeClassName="selected">
+              Create &amp; Manage Cards
+              </NavLink>
+            </li>
             <li><a href="/">Study Buffalo Home</a></li>
             <li><a href="/accounts/logout/?next=/">Logout</a></li>
           </ul>
@@ -41,13 +57,22 @@ class Menu extends React.Component {
   }
 }
 
-function Dashboard(prop) {
+function Dashboard() {
   return (
-    <h1>Study Buffalo Flashcards</h1>
+    <React.Fragment>
+      <h1>Study Buffalo Flashcards</h1>
+      <DashboardNews />
+    </React.Fragment>
   )
 }
 
-function Decks(prop) {
+function DashboardNews() {
+  return (
+    <div>News &amp; Announcements</div>
+  )
+}
+
+function Decks() {
   return (
     <Switch>
       <Route exact path="/flash-cards/decks/" component={DecksDashboard} />
@@ -56,24 +81,24 @@ function Decks(prop) {
   )
 }
 
-function DecksDashboard(prop) {
+function DecksDashboard() {
   return (
     <h1>Flash Card Decks</h1>
   )
 }
 
-function DecksCreate(prop) {
+function DecksCreate() {
   return (
     <h1>Create new deck</h1>
   )
 }
 
-function Cards(prop) {
+function Cards() {
   return (
     <h1>Flash Card Management</h1>
   )
 }
-function Main(prop) {
+function Main() {
   return (
     <Switch>
       <Route exact path="/flash-cards/" component={Dashboard} />
@@ -83,26 +108,12 @@ function Main(prop) {
   )
 }
 
-// class Main extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <Switch>
-//         <Route exact path="/" component={Dashboard} />
-//         <Route exact path="/decks" component={Decks} />
-//       </Switch>
-//     )
-//   }
-// }
-function App(prop) {
+function App() {
   return (
-    <div>
+    <React.Fragment>
       <Menu />
       <Main />
-    </div>
+    </React.Fragment>
   )
 }
 
@@ -112,5 +123,5 @@ ReactDOM.render(
       <App name="Josh" />
     </BrowserRouter>
   ),
-  document.getElementById('root')
+  document.getElementById('app')
 );
