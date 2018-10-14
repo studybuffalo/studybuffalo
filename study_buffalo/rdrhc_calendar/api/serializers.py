ddeb3c1d0752fb089461816b918aa08e347c3738
studybuffalo <studybuffalo@gmail.com>
2018-10-14 14:12:12 -0600

    Initial working API

---
+++ study_buffalo/rdrhc_calendar/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from rdrhc_calendar.models import CalendarUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CalendarUser
+        fields = (
+            'id', 'sb_user', 'schedule_name', 'calendar_name', 'role',
+            'first_email_sent', 'full_day', 'reminder',
+        )
--- study_buffalo/rdrhc_calendar/urls.py
+++ study_buffalo/rdrhc_calendar/urls.py
@@ -1,15 +1,19 @@
-from django.conf.urls import url
+from django.conf.urls import include
+from django.urls import path, re_path
 
 from . import views
 
+app_name = 'rdrhc_calendar'
+
 urlpatterns = [
-    url(r"^settings/$", views.calendar_settings, name="calendar_settings"),
-    url(r"^shifts/$", views.ShiftCodeList.as_view(), name="calendar_code_list"),
-    url(r"^shifts/(?P<code>\w+)$", views.calendar_code_edit, name="calendar_code_edit"),
-    url(r"^shifts/add/$", views.calendar_code_add, name="calendar_code_add"),
-    url(r"^shifts/delete/(?P<code>\w+)$", views.calendar_code_delete, name="calendar_code_delete"),
-    url(r"^missing-codes/$", views.MissingShiftCodeList.as_view(), name="calendar_missing_code_list"),
-    url(r"^missing-codes/add/(?P<code_id>\d+)$", views.missing_code_add, name="calendar_missing_code_add"),
-    url(r"^missing-codes/delete/(?P<code_id>\d+)$", views.missing_code_delete, name="calendar_missing_code_delete"),
-    url(r"^$", views.calendar_index, name="calendar_index"),
+    path('settings/', views.calendar_settings, name='calendar_settings'),
+    path('shifts/', views.ShiftCodeList.as_view(), name='calendar_code_list'),
+    re_path(r'^shifts/<int:code>/', views.calendar_code_edit, name='calendar_code_edit'),
+    path('shifts/add/', views.calendar_code_add, name='calendar_code_add'),
+    re_path(r'^shifts/delete/<int:code>/', views.calendar_code_delete, name='calendar_code_delete'),
+    path('missing-codes/', views.MissingShiftCodeList.as_view(), name='calendar_missing_code_list'),
+    re_path(r'missing-codes/add/<int:code_id>/', views.missing_code_add, name='calendar_missing_code_add'),
+    re_path(r'missing-codes/delete/<int:code_id>/', views.missing_code_delete, name='calendar_missing_code_delete'),
+    path('api/v1/', include('rdrhc_calendar.api.urls', namespace='api_v1')),
+    path('', views.calendar_index, name='calendar_index'),
 ]
