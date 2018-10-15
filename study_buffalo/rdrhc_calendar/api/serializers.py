6c01f85f7d586e6b01fc98f9f0fe9cc6437c5e7d
studybuffalo <studybuffalo@gmail.com>
2018-10-14 20:55:31 -0600

    Adding a shiftcode serializer and view

--- study_buffalo/rdrhc_calendar/api/serializers.py
+++ study_buffalo/rdrhc_calendar/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from rdrhc_calendar.models import CalendarUser
+from rdrhc_calendar.models import CalendarUser, ShiftCode
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,4 +8,19 @@ class UserSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'sb_user', 'name', 'schedule_name', 'calendar_name', 'role',
             'first_email_sent', 'full_day', 'reminder',
+        )
+
+class ShiftCodesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShiftCode
+        fields = (
+            'code', 'sb_user', 'role',
+            'monday_start', 'monday_duration',
+            'tuesday_start', 'tuesday_duration'
+            'wednesday_start', 'wednesday_duration'
+            'thursday_start', 'thursday_duration'
+            'friday_start', 'friday_duration'
+            'saturday_start', 'saturday_duration'
+            'sunday_start', 'sunday_duration'
+            'stat_start', 'stat_duration'
         )
