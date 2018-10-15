8b4c0da42dc9e84f737f704674fae00cca5cb265
studybuffalo <studybuffalo@gmail.com>
2018-10-14 19:32:35 -0600

    Adding rest framework authentication as token

--- study_buffalo/rdrhc_calendar/api/urls.py
+++ study_buffalo/rdrhc_calendar/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('authentication/', include('rest_framework.urls', namespace='rest_framework')),
     path('users/', views.UserList.as_view(), name='user_list'),
     re_path('users/<int:id>/', views.UserDetail.as_view(), name='user_detail'),
-
+    re_path('shift-codes/<int:id>', views.UserShiftCodesList.as_view, name='user_shift_codes_list'),
     path('', views.api_root, name='root'),
 ]
 
