ef7e16adfbd70c37175af72c87cead2a54b368b2
studybuffalo <studybuffalo@gmail.com>
2018-10-15 16:36:16 -0600

    Adding view and serializer to retrieve a users schedule

--- study_buffalo/rdrhc_calendar/api/urls.py
+++ study_buffalo/rdrhc_calendar/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include
-from django.urls import path, re_path
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from rdrhc_calendar.api import views
@@ -9,6 +9,7 @@ urlpatterns = [
     path('authentication/', include('rest_framework.urls', namespace='rest_framework')),
     path('users/', views.UserList.as_view(), name='user_list'),
     path('users/<int:id>/', views.UserDetail.as_view(), name='user_detail'),
+    path('shifts/<int:id>/', views.UserScheduleList.as_view(), name='user_schedule_list'),
     path('shift-codes/<int:id>/', views.UserShiftCodesList.as_view(), name='user_shift_codes_list'),
     path('stat-holidays/', views.StatHolidaysList.as_view(), name='stat_holidays_list'),
     path('', views.api_root, name='root'),
