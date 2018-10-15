2dfa3359b7816bceafb7ff8f242bd8e1e582ca0f
studybuffalo <studybuffalo@gmail.com>
2018-10-14 20:50:38 -0600

    Fixing path (doesn't use regex)

--- study_buffalo/rdrhc_calendar/api/urls.py
+++ study_buffalo/rdrhc_calendar/api/urls.py
@@ -8,8 +8,8 @@ app_name = 'api_v1'
 urlpatterns = [
     path('authentication/', include('rest_framework.urls', namespace='rest_framework')),
     path('users/', views.UserList.as_view(), name='user_list'),
-    re_path('users/<int:id>/', views.UserDetail.as_view(), name='user_detail'),
-    re_path('shift-codes/<int:id>', views.UserShiftCodesList.as_view, name='user_shift_codes_list'),
+    path('users/<int:id>/', views.UserDetail.as_view(), name='user_detail'),
+    path('shift-codes/<int:id>/', views.UserShiftCodesList.as_view(), name='user_shift_codes_list'),
     path('', views.api_root, name='root'),
 ]
 
