1cd158308ea6faa4996702222a5a38a2d08c7699
studybuffalo <studybuffalo@gmail.com>
2018-10-20 16:54:34 -0600

    Simplifying code_edit view, adding tests

--- study_buffalo/rdrhc_calendar/views.py
+++ study_buffalo/rdrhc_calendar/views.py
@@ -1,15 +1,15 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 from .forms import CalendarSettingsForm, CalendarShiftCodeForm, MissingCodeForm
 from .models import CalendarUser, ShiftCode, MissingShiftCode
 
-
+@login_required
 @permission_required("rdrhc_calendar.can_view", raise_exception=True)
 def calendar_index(request):
     """View for the tool page"""
@@ -19,6 +19,7 @@ def calendar_index(request):
         context={},
     )
 
+@login_required
 @permission_required("rdrhc_calendar.can_view", raise_exception=True)
 def calendar_settings(request):
     user_settings = get_object_or_404(CalendarUser, sb_user=request.user.id)
@@ -50,115 +51,41 @@ def calendar_settings(request):
 class ShiftCodeList(PermissionRequiredMixin, generic.ListView):
     permission_required = "rdrhc_calendar.can_view"
     context_object_name = "shift_code_list"
-    template_name = "shiftcode_list.html"
+    template_name = "rdrhd_calendar/shiftcode_list.html"
 
     def get_queryset(self):
-        return ShiftCode.objects.filter(sb_user=self.request.user)
+        # Confirm user has a CalenderUser instance
+        if CalendarUser.objects.filter(sb_user=self.request.user).exists():
+            return ShiftCode.objects.filter(sb_user=self.request.user)
 
+        raise Http404
+
+@login_required
 @permission_required("rdrhc_calendar.can_view", raise_exception=True)
 def calendar_code_edit(request, code_id):
     # Get the Shift Code instance for this user
-    shift_code_instance = get_object_or_404(ShiftCode, id=code_id, sb_user=request.user.id)
+    shift_code_instance = get_object_or_404(
+        ShiftCode,
+        id=code_id,
+        sb_user=request.user.id
+    )
 
     # If this is a POST request then process the Form data
     if request.method == 'POST':
-
-        # Create a form instance and populate it with data from the request (binding):
+        # Create the form
         form = CalendarShiftCodeForm(request.POST, instance=shift_code_instance)
 
         # Check if the form is valid:
         if form.is_valid():
-            # Collect the form fields
-            shift_code = form.cleaned_data["code"]
-            monday_start = form.cleaned_data["monday_start"]
-            monday_duration = form.cleaned_data["monday_duration"]
-            tuesday_start = form.cleaned_data["tuesday_start"]
-            tuesday_duration = form.cleaned_data["tuesday_duration"]
-            wednesday_start = form.cleaned_data["wednesday_start"]
-            wednesday_duration = form.cleaned_data["wednesday_duration"]
-            thursday_start = form.cleaned_data["thursday_start"]
-            thursday_duration = form.cleaned_data["thursday_duration"]
-            friday_start = form.cleaned_data["friday_start"]
-            friday_duration = form.cleaned_data["friday_duration"]
-            saturday_start = form.cleaned_data["saturday_start"]
-            saturday_duration = form.cleaned_data["saturday_duration"]
-            sunday_start = form.cleaned_data["sunday_start"]
-            sunday_duration = form.cleaned_data["sunday_duration"]
-            stat_start = form.cleaned_data["stat_start"]
-            stat_duration = form.cleaned_data["stat_duration"]
+            shift_code_instance.save()
 
-            # Check if this is a unique entry
-            sb_user = request.user.id
-            role = shift_code_instance.role
-
-            if ShiftCode.objects.filter(sb_user=sb_user, role=role, code=shift_code):
-                messages.error(request, "This shift code already exists")
-            else:
-                # Upate the user settings
-                shift_code_instance.code = shift_code
-                shift_code_instance.monday_start = monday_start
-                shift_code_instance.monday_duration = monday_duration
-                shift_code_instance.tuesday_start = tuesday_start
-                shift_code_instance.tuesday_duration = tuesday_duration
-                shift_code_instance.wednesday_start = wednesday_start
-                shift_code_instance.wednesday_duration = wednesday_duration
-                shift_code_instance.thursday_start = thursday_start
-                shift_code_instance.thursday_duration = thursday_duration
-                shift_code_instance.friday_start = friday_start
-                shift_code_instance.friday_duration = friday_duration
-                shift_code_instance.saturday_start = saturday_start
-                shift_code_instance.saturday_duration = saturday_duration
-                shift_code_instance.sunday_start = sunday_start
-                shift_code_instance.sunday_duration = sunday_duration
-                shift_code_instance.stat_start = stat_start
-                shift_code_instance.stat_duration = stat_duration
-
-                shift_code_instance.save()
-
-                # redirect to a new URL:
-                messages.success(request, "Shift code updated")
-                return HttpResponseRedirect(reverse('rdrhc_calendar:code_list'))
+            # redirect to a new URL:
+            messages.success(request, "Shift code updated")
+            return HttpResponseRedirect(reverse('rdrhc_calendar:code_list'))
 
     # If this is a GET (or any other method) create the default form
     else:
-        # Set the default form values
-        shift_code = shift_code_instance.code
-        monday_start = shift_code_instance.monday_start
-        monday_duration = shift_code_instance.monday_duration
-        tuesday_start = shift_code_instance.tuesday_start
-        tuesday_duration = shift_code_instance.tuesday_duration
-        wednesday_start = shift_code_instance.wednesday_start
-        wednesday_duration = shift_code_instance.wednesday_duration
-        thursday_start = shift_code_instance.thursday_start
-        thursday_duration = shift_code_instance.thursday_duration
-        friday_start = shift_code_instance.friday_start
-        friday_duration = shift_code_instance.friday_duration
-        saturday_start = shift_code_instance.saturday_start
-        saturday_duration = shift_code_instance.saturday_duration
-        sunday_start = shift_code_instance.sunday_start
-        sunday_duration = shift_code_instance.sunday_duration
-        stat_start = shift_code_instance.stat_start
-        stat_duration = shift_code_instance.stat_duration
-
-        form = CalendarShiftCodeForm(initial={
-            "code": shift_code,
-            "monday_start": monday_start,
-            "monday_duration": monday_duration,
-            "tuesday_start": tuesday_start,
-            "tuesday_duration": tuesday_duration,
-            "wednesday_start": wednesday_start,
-            "wednesday_duration": wednesday_duration,
-            "thursday_start": thursday_start,
-            "thursday_duration": thursday_duration,
-            "friday_start": friday_start,
-            "friday_duration": friday_duration,
-            "saturday_start": saturday_start,
-            "saturday_duration": saturday_duration,
-            "sunday_start": sunday_start,
-            "sunday_duration": sunday_duration,
-            "stat_start": stat_start,
-            "stat_duration": stat_duration,
-        })
+        form = CalendarShiftCodeForm(instance=shift_code_instance)
 
     return render(
         request,
@@ -166,6 +93,7 @@ def calendar_code_edit(request, code_id):
         {'form': form}
     )
 
+@login_required
 @permission_required("rdrhc_calendar.can_view", raise_exception=True)
 def calendar_code_add(request):
     if request.method == 'POST':
@@ -240,6 +168,7 @@ def calendar_code_add(request):
         {'form': form}
     )
 
+@login_required
 @permission_required("rdrhc_calendar.can_view", raise_exception=True)
 def calendar_code_delete(request, code):
     # Get the Shift Code instance for this user
@@ -267,6 +196,7 @@ class MissingShiftCodeList(PermissionRequiredMixin, generic.ListView):
     def get_queryset(self):
         return MissingShiftCode.objects.all()
 
+@login_required
 @permission_required("rdrhc_calendar.can_add_default_codes", raise_exception=True)
 def missing_code_add(request, code_id):
     # Get the Shift Code instance for this user
@@ -351,6 +281,7 @@ def missing_code_add(request, code_id):
         }
     )
 
+@login_required
 @permission_required("rdrhc_calendar.can_add_default_codes", raise_exception=True)
 def missing_code_delete(request, codeid):
     # Get the Shift Code instance for this user
