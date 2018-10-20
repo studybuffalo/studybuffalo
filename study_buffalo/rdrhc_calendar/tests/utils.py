35adce074b3a76cd2ed3a7ad53118c6a99040bf1
studybuffalo <studybuffalo@gmail.com>
2018-10-20 16:53:57 -0600

    Moving more user creation things into utilities

--- study_buffalo/rdrhc_calendar/tests/utils.py
+++ study_buffalo/rdrhc_calendar/tests/utils.py
@@ -1,5 +1,10 @@
 """Utility functions for testing."""
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+from rdrhc_calendar import models
+
 
 def create_user(username):
     user = get_user_model().objects.create()
@@ -9,5 +14,38 @@ def create_user(username):
     user.is_staff = False
     user.is_active = True
     user.save()
+
+    return user
+
+def create_user_with_permission(username):
+    # Create user
+    user = create_user(username)
+
+    # Add permission
+    content_type = ContentType.objects.get_for_model(models.CalendarUser)
+    user.user_permissions.add(
+        Permission.objects.get(content_type=content_type, codename='can_view')
+    )
+
+    return user
+
+def create_user_with_permission_calendar(username):
+    # Create user
+    user = create_user(username)
+
+    # Add permission
+    content_type = ContentType.objects.get_for_model(models.CalendarUser)
+    user.user_permissions.add(
+        Permission.objects.get(content_type=content_type, codename='can_view')
+    )
+
+    # Add CalendarUser to user
+    models.CalendarUser.objects.create(
+        sb_user=user,
+        name='Regular User',
+        schedule_name='User',
+        calendar_name='SecretCalendar',
+        role='p',
+    )
 
     return user
