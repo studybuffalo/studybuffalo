f37b16f04c07c950fd1fa6dc6619b633a4cc5ec0
studybuffalo <studybuffalo@gmail.com>
2018-10-20 14:54:37 -0600

    Updating create_user function; updating model tests to accomodate new arguments

--- study_buffalo/rdrhc_calendar/tests/utils.py
+++ study_buffalo/rdrhc_calendar/tests/utils.py
@@ -1,9 +1,9 @@
 """Utility functions for testing."""
 from django.contrib.auth import get_user_model
 
-def create_user():
+def create_user(username):
     user = get_user_model().objects.create()
-    user.username = 'Regular User'
+    user.username = username
     user.set_password('abcd123456')
     user.is_superuser = False
     user.is_staff = False
