9bcb5319a43a70382676e3a544e179ce0128090f
studybuffalo <studybuffalo@gmail.com>
2018-10-20 12:13:02 -0600

    Creating an API permission to restrict access

--- study_buffalo/rdrhc_calendar/models.py
+++ study_buffalo/rdrhc_calendar/models.py
@@ -11,48 +11,48 @@ class CalendarUser(models.Model):
     )
 
     name = models.CharField(
-        help_text="The name of the user",
+        help_text='The name of the user',
         max_length=25,
     )
 
     schedule_name = models.CharField(
-        help_text="The name of the user on the Excel schedule",
+        help_text='The name of the user on the Excel schedule',
         max_length=25,
     )
 
     calendar_name = models.CharField(
-        help_text="The name of your calendar file",
+        help_text='The name of your calendar file',
         max_length=50,
         unique=True,
     )
 
     role = models.CharField(
         choices=(
-            ("a", "Pharmacy Assistant"),
-            ("p", "Pharmacist"),
-            ("t", "Pharmacy Technician"),
+            ('a', 'Pharmacy Assistant'),
+            ('p', 'Pharmacist'),
+            ('t', 'Pharmacy Technician'),
         ),
-        help_text="The role/profession of the user",
+        help_text='The role/profession of the user',
         max_length=1,
     )
 
     first_email_sent = models.BooleanField(
         default=False,
-        help_text="Whether the welcome email has been sent or not",
+        help_text='Whether the welcome email has been sent or not',
     )
 
     full_day = models.BooleanField(
         default=False,
         help_text=(
-            "Whether the .ics calender should show shifts as "
-            "full day events or not"
+            'Whether the .ics calender should show shifts as '
+            'full day events or not'
         ),
     )
 
     reminder = models.IntegerField(
         help_text=(
-            "How much time before the shift starts (in minutes) to "
-            "provide a reminder (leave blank for no reminder)"
+            'How much time before the shift starts (in minutes) to '
+            'provide a reminder (leave blank for no reminder)'
         ),
         blank=True,
         null=True
@@ -60,27 +60,28 @@ class CalendarUser(models.Model):
 
     class Meta:
         permissions = (
-            ("can_view", "Can view the RDRHC Calendar settings view"),
+            ('can_view', 'Can view the RDRHC Calendar settings view'),
+            ('access_api', 'Can access all API endpoints')
         )
-        verbose_name = "RDRHC Calendar User"
-        verbose_name_plural = "RDRHC Calendar Users"
+        verbose_name = 'RDRHC Calendar User'
+        verbose_name_plural = 'RDRHC Calendar Users'
 
     def __str__(self):
-        return "{0} - {1}".format(self.role, self.name)
+        return '{0} - {1}'.format(self.role, self.name)
 
 class StatHoliday(models.Model):
     date = models.DateField()
 
     class Meta:
-        verbose_name = "Stat Holiday"
+        verbose_name = 'Stat Holiday'
         verbose_name_plural = "Stat Holidays'"
 
     def __str__(self):
-        return "{0}".format(self.date)
+        return '{0}'.format(self.date)
 
 class ShiftCode(models.Model):
     code = models.CharField(
-        help_text="The shift code used in the Excel schedule",
+        help_text='The shift code used in the Excel schedule',
         max_length=20,
     )
 
@@ -88,8 +89,8 @@ class ShiftCode(models.Model):
         settings.AUTH_USER_MODEL,
         blank=True,
         help_text=(
-            "Which user this shift code applies to (leave blank for "
-            "a default entry)"
+            'Which user this shift code applies to (leave blank for '
+            'a default entry)'
         ),
         null=True,
         on_delete=models.CASCADE,
@@ -97,11 +98,11 @@ class ShiftCode(models.Model):
 
     role = models.CharField(
         choices=(
-            ("a", "Pharmacy Assistant"),
-            ("p", "Pharmacist"),
-            ("t", "Pharmacy Technician"),
+            ('a', 'Pharmacy Assistant'),
+            ('p', 'Pharmacist'),
+            ('t', 'Pharmacy Technician'),
         ),
-        help_text="The role/profession this code applies to",
+        help_text='The role/profession this code applies to',
         max_length=1,
     )
 
@@ -109,8 +110,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -119,7 +120,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -128,8 +129,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -138,7 +139,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -147,8 +148,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -157,7 +158,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -166,8 +167,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -176,7 +177,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -185,8 +186,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -195,7 +196,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -204,8 +205,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -214,7 +215,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -223,8 +224,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -233,7 +234,7 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
@@ -242,8 +243,8 @@ class ShiftCode(models.Model):
         blank=True,
         default=time(7, 0),
         help_text=(
-            "The start time of the shift (leave blank if there should "
-            "be no shift at this time)"
+            'The start time of the shift (leave blank if there should '
+            'be no shift at this time)'
         ),
         null=True,
     )
@@ -252,73 +253,73 @@ class ShiftCode(models.Model):
         blank=True,
         decimal_places=2,
         default=15.00,
-        help_text="The duration of the shift in hours (defaults to 15 hours)",
+        help_text='The duration of the shift in hours (defaults to 15 hours)',
         max_digits=4,
         null=True,
     )
 
     class Meta:
-        unique_together = ("code", "sb_user", "role",)
-        verbose_name = "Shift Code"
-        verbose_name_plural = "Shift Codes"
+        unique_together = ('code', 'sb_user', 'role',)
+        verbose_name = 'Shift Code'
+        verbose_name_plural = 'Shift Codes'
 
     def __str__(self):
         if self.sb_user:
-            return "{0} - {1} - {2}".format(
+            return '{0} - {1} - {2}'.format(
                 self.get_role_display(), self.sb_user, self.code
             )
         else:
-            return "{0} - {1}".format(self.get_role_display(), self.code)
+            return '{0} - {1}'.format(self.get_role_display(), self.code)
 
 class Shift(models.Model):
     sb_user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        help_text="The user this shift applies to",
+        help_text='The user this shift applies to',
         on_delete=models.CASCADE,
     )
 
     date = models.DateField(
-        help_text="The date of the shift",
+        help_text='The date of the shift',
     )
 
     shift_code = models.ForeignKey(
         ShiftCode,
         blank=True,
-        help_text="The shift code for this shift",
+        help_text='The shift code for this shift',
         null=True,
         on_delete=models.CASCADE,
     )
 
     text_shift_code = models.CharField(
-        help_text="Text representation of the shift code",
+        help_text='Text representation of the shift code',
         max_length=20,
     )
 
     def __str__(self):
-        return "{0} - {1}".format(self.date, self.shift_code)
+        return '{0} - {1}'.format(self.date, self.shift_code)
 
 class MissingShiftCode(models.Model):
     code = models.CharField(
-        help_text="The shift code used in the Excel schedule",
+        help_text='The shift code used in the Excel schedule',
         max_length=20,
     )
 
     role = models.CharField(
         choices=(
-            ("a", "Pharmacy Assistant"),
-            ("p", "Pharmacist"),
-            ("t", "Pharmacy Technician"),
+            ('a', 'Pharmacy Assistant'),
+            ('p', 'Pharmacist'),
+            ('t', 'Pharmacy Technician'),
         ),
-        help_text="The role/profession this code applies to",
+        help_text='The role/profession this code applies to',
         max_length=1,
     )
 
     def __str__(self):
-        return "{0} - {1}".format(self.code, self.role)
+        return '{0} - {1}'.format(self.code, self.role)
 
     class Meta:
         permissions = (
-            ("can_add_default_codes",
-             "Can add new default codes to the ShiftCode database"),
+            ('can_add_default_codes',
+             'Can add new default codes to the ShiftCode database'),
         )
-        unique_together = ["code", "role"]
+        unique_together = ['code', 'role']
