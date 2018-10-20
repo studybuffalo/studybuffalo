acab321964c7462bc6a7ab52da630ee32b33d96c
studybuffalo <studybuffalo@gmail.com>
2018-10-20 16:54:12 -0600

    Switching to single quotation

--- study_buffalo/rdrhc_calendar/forms.py
+++ study_buffalo/rdrhc_calendar/forms.py
@@ -4,19 +4,19 @@ from .models import CalendarUser, ShiftCode
 class CalendarSettingsForm(forms.ModelForm):
     full_day = forms.ChoiceField(
         choices=(
-            ("False", "No"),
-            ("True", "Yes"),
+            ('False', 'No'),
+            ('True', 'Yes'),
         ),
         help_text=(
-            "Whether your schedule should display your shifts as "
-            "full day events or not"
+            'Whether your schedule should display your shifts as '
+            'full day events or not'
         ),
     )
 
     reminder = forms.IntegerField(
         help_text=(
-            "How much time before the shift to display a calendar "
-            "reminder (leave blank for no reminder)"
+            'How much time before the shift to display a calendar '
+            'reminder (leave blank for no reminder)'
         ),
         max_value=60,
         min_value=0,
@@ -26,57 +26,57 @@ class CalendarSettingsForm(forms.ModelForm):
     class Meta:
         model = CalendarUser
         fields = [
-            "calendar_name",
-            "full_day",
-            "reminder",
+            'calendar_name',
+            'full_day',
+            'reminder',
         ]
 
 class CalendarShiftCodeForm(forms.ModelForm):
     code = forms.CharField(
-        help_text="The shift code used in the Excel schedule",
+        help_text='The shift code used in the Excel schedule',
         max_length=20,
     )
 
     class Meta:
         model = ShiftCode
         fields = [
-            "code",
-            "monday_start",
-            "monday_duration",
-            "tuesday_start",
-            "tuesday_duration",
-            "wednesday_start",
-            "wednesday_duration",
-            "thursday_start",
-            "thursday_duration",
-            "friday_start",
-            "friday_duration",
-            "saturday_start",
-            "saturday_duration",
-            "sunday_start",
-            "sunday_duration",
-            "stat_start",
-            "stat_duration",
+            'code',
+            'monday_start',
+            'monday_duration',
+            'tuesday_start',
+            'tuesday_duration',
+            'wednesday_start',
+            'wednesday_duration',
+            'thursday_start',
+            'thursday_duration',
+            'friday_start',
+            'friday_duration',
+            'saturday_start',
+            'saturday_duration',
+            'sunday_start',
+            'sunday_duration',
+            'stat_start',
+            'stat_duration',
         ]
-        
+
 class MissingCodeForm(forms.ModelForm):
     class Meta:
         model = ShiftCode
         fields = [
-            "monday_start",
-            "monday_duration",
-            "tuesday_start",
-            "tuesday_duration",
-            "wednesday_start",
-            "wednesday_duration",
-            "thursday_start",
-            "thursday_duration",
-            "friday_start",
-            "friday_duration",
-            "saturday_start",
-            "saturday_duration",
-            "sunday_start",
-            "sunday_duration",
-            "stat_start",
-            "stat_duration",
+            'monday_start',
+            'monday_duration',
+            'tuesday_start',
+            'tuesday_duration',
+            'wednesday_start',
+            'wednesday_duration',
+            'thursday_start',
+            'thursday_duration',
+            'friday_start',
+            'friday_duration',
+            'saturday_start',
+            'saturday_duration',
+            'sunday_start',
+            'sunday_duration',
+            'stat_start',
+            'stat_duration',
         ]
