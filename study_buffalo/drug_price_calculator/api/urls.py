a6b2aa3894255d3ef5de822ba8b96cd3598e024e
studybuffalo <studybuffalo@gmail.com>
2018-10-21 20:57:24 -0600

    Adding basic plan for creating API endpoints

--- study_buffalo/drug_price_calculator/api/urls.py
+++ study_buffalo/drug_price_calculator/api/urls.py
@@ -10,3 +10,28 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+"""API Endpoint Planner
+
+- Currently removes data from database - can we keep a historical record?
+  - Simple history?
+- Uploads:
+  - ATC
+  - Coverage
+  - Extra data
+  - Price data
+  - PTC
+  - Special authorization
+
+
+OTHER
+  - Will need an API as well for the substitution application
+  - Need to send data for substitution screening
+    - BSRF
+    - Generic
+    - Manufacturer
+    - PTC
+    -
+  - Need to request list of data for substitutions
+
+"""
