9a4388fa276295a7688e87d547fb4abecaf895f6
studybuffalo <studybuffalo@gmail.com>
2018-12-10 22:57:54 -0700

    Removing unused code

--- config/settings/test.py
+++ config/settings/test.py
@@ -56,6 +56,3 @@ EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 EMAIL_HOST = "localhost"
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = 1025
-
-# Your stuff...
-# ------------------------------------------------------------------------------
