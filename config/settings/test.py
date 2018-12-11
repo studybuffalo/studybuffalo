b7100c129303cb0f3cfa5819b893c812fe333d8b
studybuffalo <studybuffalo@gmail.com>
2018-12-10 22:56:44 -0700

    Enabling template debugging for coverage plugin

--- config/settings/test.py
+++ config/settings/test.py
@@ -2,7 +2,7 @@
 With these settings, tests run faster.
 """
 
-from .base import *  # noqa
+from .base import * # pylint: disable=wildcard-import, unused-wildcard-import
 from .base import env
 
 # GENERAL
@@ -37,8 +37,8 @@ PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 # TEMPLATES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
-TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG  # noqa F405
-TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
+TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
+TEMPLATES[0]["OPTIONS"]["loaders"] = [
     (
         "django.template.loaders.cached.Loader",
         [
@@ -47,6 +47,7 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
         ],
     )
 ]
+TEMPLATE_DEBUG = True
 
 # EMAIL
 # ------------------------------------------------------------------------------
