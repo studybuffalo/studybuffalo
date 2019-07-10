88e4b01df3e540de2ff3d965c09f34d3df62e145
studybuffalo <studybuffalo@gmail.com>
2019-07-09 22:45:51 -0600

    Fixing up BSRF and Generic parsing

--- study_buffalo/api/drug_price_calculator/parse.py
+++ study_buffalo/api/drug_price_calculator/parse.py
@@ -1,171 +1,169 @@
 """Functions to parse iDBL data for insertion into database."""
 import re
 
+from sentry_sdk import capture_message
 from drug_price_calculator import models
 
 
 def parse_bsrf(bsrf):
-    """
-    # Checks if the text has a substitution
-    # Remove extra white space from searchText
-    searchText = re.sub(r'\s{2,}', ' ', text)
-    sub = binary_search(searchText, bsrfSubs)
+    """Parses the raw Brand Name, Strength, Routh, Dosage Form data."""
+    # Get substitution or create pending model
+    try:
+        sub = models.SubsBSRF.objects.get(bsrf=bsrf)
+        pend = None
+    except models.SubsBSRF.DoesNotExist:
+        sub = None
+        pend, _ = models.PendBSRF.objects.get_or_create(original=bsrf)
 
     if sub:
-        brandName = sub.brandName
-        strength = sub.strength
-        route = sub.route
-        dosageForm = sub.dosageForm
-        matched = True
+        return {
+            'brand_name': sub.brand_name,
+            'strength': sub.strength,
+            'route': sub.route,
+            'dosage_form': sub.dosage_form,
+        }
 
-    # If no substitution, apply regular processing
-    else:
-        # Splits text multiple strings depending on the format
-        # used
-        # Formats vary with number of white space between
-        # sections
-        match3 = r'\S\s{3}\S'
-        match4 = r'\S\s{4}\S'
+    # Splits text multiple strings depending on the format
+    # used
+    # Formats vary with number of white space between
+    # sections
+    match3 = r'\S\s{3}\S'
+    match4 = r'\S\s{4}\S'
 
-        # Format: B S    R   F
-        if re.search(match4, text) and re.search(match3, text):
-            try:
-                text = text.split('   ')
+    # Format: B S    R   F
+    if re.search(match4, bsrf) and re.search(match3, bsrf):
+        try:
+            text = bsrf.split('   ')
 
-                brandStrength = text[0].strip()
-                route = text[1].strip()
-                dosageForm = text[2].strip()
-            except Exception:
-                log.critical('URL %s: Error extracting BSRF', url)
+            brand_strength = text[0].strip()
+            route = text[1].strip()
+            dosage_form = text[2].strip()
+        except IndexError:
+            capture_message(
+                message='Error processing "{}"'.format(bsrf), level=30
+            )
 
-                brandStrength = text
-                route = None
-                dosageForm = None
-
-        # Format: B S    F
-        elif re.search(match4, text):
-            try:
-                text = text.split('    ')
-
-                brandStrength = text[0].strip()
-                route = None
-                dosageForm = text[1].strip()
-            except Exception:
-                log.critical(
-                    'URL %s: Error extracting 4 space BSF', url
-                )
-
-                brandStrength = text
-                route = None
-                dosageForm = None
-
-        # Format: B S   F   or   B R   F
-        # Note: cannot properly extract the B R   F cases
-        elif re.search(match3, text):
-            try:
-                text = text.split('   ')
-
-                brandStrength = text[0].strip()
-                route = None
-                dosageForm = text[1].strip()
-            except Exception:
-                log.critical(
-                    'URL %s: Error extracting 3 space BSF', url
-                )
-
-                brandStrength = text
-                route = None
-                dosageForm = None
-
-        # Format: B S
-        else:
-            # B S
-            brandStrength = text
+            brand_strength = bsrf
             route = None
-            dosageForm = None
+            dosage_form = None
 
-        # Splits the brandStrength at the first number
-        # encountered with a non-numeric character behind it
-        # (excluding commas in numbers, as this is assumed
-        # to be a unit)
-        search = re.search(r'\s\b[0-9,]+\D+', brandStrength)
+    # Format: B S    F
+    elif re.search(match4, bsrf):
+        try:
+            text = bsrf.split('    ')
 
-        if search:
-            split = search.start()
+            brand_strength = text[0].strip()
+            route = None
+            dosage_form = text[1].strip()
+        except IndexError:
+            capture_message(
+                message='Error processing "{}"'.format(bsrf), level=30
+            )
 
-            brandName = brandStrength[:split].strip()
-            strength = brandStrength[split:].strip()
-        else:
-            brandName = brandStrength
-            strength = None
+            brand_strength = bsrf
+            route = None
+            dosage_form = None
 
-        # Apply final corrections to extracted information
-        if brandName:
-            brandName = parse_brand_name(brandName)
+    # Format: B S   F   or   B R   F
+    # Note: cannot properly extract the B R   F cases
+    elif re.search(match3, bsrf):
+        try:
+            text = bsrf.split('   ')
 
-        if strength:
-            strength = parse_strength(strength)
+            brand_strength = text[0].strip()
+            route = None
+            dosage_form = text[1].strip()
+        except IndexError:
+            capture_message(
+                message='Error processing "{}"'.format(bsrf), level=30
+            )
 
-        if route:
-            route = parse_route(route)
+            brand_strength = bsrf
+            route = None
+            dosage_form = None
 
-        if dosageForm:
-            dosageForm = parse_dosage_form(dosageForm)
+    # Format: B S
+    else:
+        # B S
+        brand_strength = bsrf
+        route = None
+        dosage_form = None
 
-        # Flags html as not having sub match
-        matched = False
+    # Splits the brand_strength at the first number
+    # encountered with a non-numeric character behind it
+    # (excluding commas in numbers, as this is assumed
+    # to be a unit)
+    search = re.search(r'\s\b[0-9,]+\D+', brand_strength)
 
+    if search:
+        split = search.start()
 
-    output = BSRF(brandName, strength, route, dosageForm,
-                  searchText, matched)
+        brand_name = brand_strength[:split].strip()
+        strength = brand_strength[split:].strip()
+    else:
+        brand_name = brand_strength
+        strength = None
 
-    return output
-    """
+    # Apply final corrections to extracted information
+    if brand_name:
+        brand_name = _parse_brand_name(brand_name)
+
+    if strength:
+        strength = _parse_strength(strength)
+
+    if route:
+        route = _parse_route(route)
+
+    if dosage_form:
+        dosage_form = _parse_dosage_form(dosage_form)
+
+    # If this is a new BSRF, add the data to the pending model
+    if pend:
+        pend.brand_name = brand_name
+        pend.strength = strength
+        pend.route = route
+        pend.dosage_form = dosage_form
+        pend.save()
+
     return {
-        'brand_name': '',
-        'strength': '',
-        'route': '',
-        'dosage_form': '',
+        'brand_name': brand_name,
+        'strength': strength,
+        'route': route,
+        'dosage_form': dosage_form,
     }
 
-def parse_generic(generic):
-    """
-        # Remove parenthesis
-        original = text[1:len(text) - 1]
+def parse_generic(raw_generic):
+    # Remove any extra white space
+    original = raw_generic.strip()
 
-        # Check if this text has a substitution
-        sub = binary_search(original, subs)
-
-        # If there is a sub, apply it
-        if sub:
-            generic = sub
-            matched = True
-
-        # Otherwise apply regular processing
-        else:
-            # Convert to lower case
-            generic = original.lower()
-
-            # Removes extra space characters
-            generic = re.sub(r'\s{2,}', ' ', generic)
-
-            # Remove spaces around slashes
-            generic = re.sub(r'/\s', '/', generic)
-
-            matched = False
-
-        return Generic(generic, original, matched)
-
+    # Get substitution or create pending model
     try:
-        genericText = html.find_all('tr', class_='idblTable')[2]\
-                            .td.div.string.strip()
+        sub = models.SubsGeneric.objects.get(original=original)
+        pend = None
+    except models.SubsGeneric.DoesNotExist:
+        sub = None
+        pend, _ = models.PendGeneric.objects.get_or_create(original=original)
 
-        generic = parse_generic(genericText)
-    except:
-        log.critical('URL %s: unable to extract generic name' % url)
-    """
+    # If subsitution present, return corrected value
+    if sub:
+        return sub.correction
 
-    return ''
+    # Otherwise apply regular processing
+    # Convert to lower case
+    generic = original.lower()
+
+    # Removes extra space characters
+    generic = re.sub(r'\s{2,}', ' ', generic)
+
+    # Remove spaces around slashes
+    generic = re.sub(r'/\s', '/', generic)
+
+    # Add data to pend if present
+    if pend:
+        pend.correction = generic
+        pend.save()
+
+    return generic
 
 def parse_manufacturer(manufacturer):
     return ''
