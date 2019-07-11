b5f0c5dafd18372b171cb73b3b567effd1fe5a1b
studybuffalo <studybuffalo@gmail.com>
2019-07-11 06:30:02 -0600

    Adding basic admin views

--- study_buffalo/drug_price_calculator/admin.py
+++ study_buffalo/drug_price_calculator/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+
+
+
+@admin.register(models.Drug)
+class DrugAdmin(admin.ModelAdmin):
+    """Admin for the Drug (and related) models."""
+    model = models.Drug
+
+    list_display = ('din', 'brand_name', 'generic_name', 'strength', 'dosage_form')
+    ordering = ('generic_name', 'strength', 'dosage_form')
+
+class ClientsInlineAdmin(admin.StackedInline):
+    model = models.Clients
+
+@admin.register(models.Price)
+class PriceAdmin(admin.ModelAdmin):
+    """Admin for the Price (and related) models."""
+    model = models.Price
+
+    list_display = ('drug', 'abc_id', 'unit_price', 'lca_price', 'mac_price')
+    ordering = ('abc_id',)
+
+    fields = (
+        'drug', 'abc_id', 'date_listed', 'unit_price', 'lca_price', 'mac_price',
+        'mac_text', 'unit_issue', 'interchangeable', 'coverage_status',
+        'clients'
+    )
