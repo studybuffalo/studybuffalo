5e68a0256b5effdfb6ff7467724c78c238370912
studybuffalo <studybuffalo@gmail.com>
2019-07-13 10:05:05 -0600

    Adding verbose names for PTC Fixing quotation marks

--- study_buffalo/drug_price_calculator/admin.py
+++ study_buffalo/drug_price_calculator/admin.py
@@ -14,8 +14,16 @@ class DrugAdmin(admin.ModelAdmin):
     list_display = ('din', 'brand_name', 'generic_name', 'strength', 'dosage_form')
     ordering = ('generic_name', 'strength', 'dosage_form')
 
-class ClientsInlineAdmin(admin.StackedInline):
+class ClientsInlineAdmin(admin.TabularInline):
     model = models.Clients
+
+class CoverageCriteriaInlineAdmin(admin.TabularInline):
+    model = models.CoverageCriteria
+    extra = 1
+
+class SpecialInlineAdmin(admin.TabularInline):
+    model = models.Price.special_authorizations.through
+    extra = 1
 
 @admin.register(models.Price)
 class PriceAdmin(admin.ModelAdmin):
@@ -29,4 +37,30 @@ class PriceAdmin(admin.ModelAdmin):
         'drug', 'abc_id', 'date_listed', 'unit_price', 'lca_price', 'mac_price',
         'mac_text', 'unit_issue', 'interchangeable', 'coverage_status',
     )
-    inlines = (ClientsInlineAdmin,)
+    inlines = (ClientsInlineAdmin, CoverageCriteriaInlineAdmin, SpecialInlineAdmin)
+
+@admin.register(models.ATC)
+class ATCAdmin(admin.ModelAdmin):
+    """Admin for the ATC model."""
+    model = models.ATC
+
+    list_display = ('id', 'atc_1', 'atc_2', 'atc_3', 'atc_4')
+    orders = ('id',)
+
+    fields = (
+        'id', 'atc_1', 'atc_1_text', 'atc_2', 'atc_2_text',
+        'atc_3', 'atc_3_text', 'atc_4', 'atc_4_text',
+    )
+
+@admin.register(models.PTC)
+class PTCAdmin(admin.ModelAdmin):
+    """Admin for the PTC model."""
+    model = models.PTC
+
+    list_display = ('id', 'ptc_1', 'ptc_2', 'ptc_3', 'ptc_4')
+    orders = ('id',)
+
+    fields = (
+        'id', 'ptc_1', 'ptc_1_text', 'ptc_2', 'ptc_2_text',
+        'ptc_3', 'ptc_3_text', 'ptc_4', 'ptc_4_text',
+    )
