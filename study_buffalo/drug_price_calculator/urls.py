19a9db5cb9ecc064e601b23f87096728c34034e5
studybuffalo <studybuffalo@gmail.com>
2019-07-15 22:26:07 -0600

    Adding view for coverage criteria

--- study_buffalo/drug_price_calculator/urls.py
+++ study_buffalo/drug_price_calculator/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     # Frontend endpoints
-    url(r"^live-search/$", views.live_search, name="live_search"),
-    url(r"^add-item/$", views.add_item, name="add_item"),
-    url(r"^comparison-search/$", views.comparison_search, name="comparison_search"),
-    url(r"^generate-comparison/$", views.generate_comparison, name="generate_comparison"),
-    url(r"^$", views.index, name="drug_price_calculator_index"),
+    path('coverage-criteria/<int:price_id>/', views.prices_coverage_criteria),
+    path('', views.index, name='drug_price_calculator_index'),
 ]
