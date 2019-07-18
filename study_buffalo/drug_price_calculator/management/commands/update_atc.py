6afc3ecddb36fa6ba5bc4ba59ae2fa69a98bbd28
studybuffalo <studybuffalo@gmail.com>
2019-07-18 07:00:30 -0600

    Cleaning up ATC extraction function

--- study_buffalo/drug_price_calculator/management/commands/update_atc.py
+++ study_buffalo/drug_price_calculator/management/commands/update_atc.py
@@ -3,6 +3,8 @@ import time
 
 from bs4 import BeautifulSoup
 import requests
+from sentry_sdk import capture_message
+from tqdm import tqdm
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -16,18 +18,18 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        # Create session to query the WHO ATC database
         session = requests.Session()
         session.headers.update({
             'User-Agent': 'Study Buffalo Data Extraction/2.0 (+https://studybuffalo.com/dataextraction/)',
             'From': 'studybuffalo@studybuffalo.com',
         })
 
-        base_url = 'https://www.whocc.no/atc_ddd_index/?code='
+        for atc in tqdm(ATC.objects.filter(atc_5__isnull=False).filter(atc_5_text__isnull=True)):
+            # Assemble URL to query
+            url = 'https://www.whocc.no/atc_ddd_index/?code={}'.format(atc.atc_5)
 
-        for atc in ATC.objects.filter(atc_5__isnull=False).filter(atc_5_text__isnull=True):
-            url = '{}{}'.format(base_url, atc.atc_5)
-            self.stdout.write('Extracting data for {}'.format(atc.atc_5))
-
+            # If valid response, can extract data
             response = session.get(url)
 
             if response.status_code == 200:
@@ -35,22 +37,22 @@ class Command(BaseCommand):
                 soup = BeautifulSoup(html, 'lxml')
 
                 try:
+                    # Extract ATC description
                     atc_text = soup.find(id='content').ul.table.find_all(
                         'tr', recursive=False,
                     )[1].find_all(
                         'td'
                     )[1].a.text.strip()
 
-                    self.stdout.write('Extracted "{}"'.format(atc_text))
-
+                    # Update the ATC record
                     atc.atc_5_text = atc_text
                     atc.save()
                 except AttributeError:
-                    self.stdout.write(
-                        'No code found for {} (reference DIN: {})'.format(
-                            atc.atc_5, atc.drugs.last().din
-                        )
+                    # Unable to find data, send message for manual follow-up
+                    message = 'No ATC found for {} (reference DIN: {})'.format(
+                        atc.atc_5, atc.drugs.last().din
                     )
+                    capture_message(message, level=20)
 
             # Sleep of 10 seconds to comply with robots.txt
             time.sleep(10)
