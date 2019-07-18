b6af00e9a5b8531a17698d8dd17452e4ff1fdcde
studybuffalo <studybuffalo@gmail.com>
2019-07-17 21:11:10 -0600

    Adding command to extract ATC level 5 data

---
+++ study_buffalo/drug_price_calculator/management/commands/update_atc.py
@@ -0,0 +1,52 @@
+
+import time
+
+from bs4 import BeautifulSoup
+import requests
+
+from django.core.management.base import BaseCommand, CommandError
+
+from drug_price_calculator.models import ATC
+
+
+class Command(BaseCommand):
+    help = 'Updates temporary ATC records with descriptions.'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        session = requests.Session()
+        session.headers.update({
+            'User-Agent': 'Study Buffalo Data Extraction/2.0 (+https://studybuffalo.com/dataextraction/)',
+            'From': 'studybuffalo@studybuffalo.com',
+        })
+
+        base_url = 'https://www.whocc.no/atc_ddd_index/?code='
+
+        for atc in ATC.objects.filter(atc_5__isnull=False).filter(atc_5_text__isnull=True):
+            url = '{}{}'.format(base_url, atc.atc_5)
+            self.stdout.write('Extracting data for {}'.format(atc.atc_5))
+
+            response = session.get(url)
+
+            if response.status_code == 200:
+                html = response.text
+                soup = BeautifulSoup(html, 'lxml')
+
+                try:
+                    atc_text = soup.find(id='content').ul.table.find_all(
+                        'tr', recursive=False,
+                    )[1].find_all(
+                        'td'
+                    )[1].a.text.strip()
+
+                    self.stdout.write('Extracted "{}"'.format(atc_text))
+
+                    atc.atc_5_text = atc_text
+                    atc.save()
+                except AttributeError:
+                    pass
+
+            # Sleep of 10 seconds to comply with robots.txt
+            time.sleep(10)
