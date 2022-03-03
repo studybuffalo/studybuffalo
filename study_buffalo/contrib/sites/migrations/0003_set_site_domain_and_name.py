b84b3439c177fb1c7b137dd730af2592b235edd0
studybuffalo <studybuffalo@gmail.com>
2022-03-03 11:26:25 -0700

    Correct string quotes

--- study_buffalo/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ study_buffalo/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -10,25 +10,25 @@ from django.db import migrations
 
 def update_site_forward(apps, schema_editor): # pylint: disable=unused-argument
     """Set site domain and name."""
-    Site = apps.get_model("sites", "Site")
+    Site = apps.get_model('sites', 'Site')
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "studybuffalo.com",
-            "name": "Study Buffalo",
+            'domain': 'studybuffalo.com',
+            'name': 'Study Buffalo',
         },
     )
 
 
 def update_site_backward(apps, schema_editor): # pylint: disable=unused-argument
     """Revert site domain and name to default."""
-    Site = apps.get_model("sites", "Site")
+    Site = apps.get_model('sites', 'Site')
     Site.objects.update_or_create(
-        id=settings.SITE_ID, defaults={"domain": "example.com", "name": "example.com"}
+        id=settings.SITE_ID, defaults={'domain': 'example.com', 'name': 'example.com'}
     )
 
 class Migration(migrations.Migration):
 
-    dependencies = [("sites", "0002_alter_domain_unique")]
+    dependencies = [('sites', '0002_alter_domain_unique')]
 
     operations = [migrations.RunPython(update_site_forward, update_site_backward)]
