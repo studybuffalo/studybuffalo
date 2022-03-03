60b19b4109642a031f9e717add48f74d5526ad69
studybuffalo <studybuffalo@gmail.com>
2022-03-03 11:43:40 -0700

    Resolving pylint errors

--- study_buffalo/play/apps.py
+++ study_buffalo/play/apps.py
@@ -1,5 +1,7 @@
+"""Application settings for the Play app."""
 from django.apps import AppConfig
 
 
 class PlayConfig(AppConfig):
+    """Configuration details for the Play app."""
     name = 'play'
--- study_buffalo/play/urls.py
+++ study_buffalo/play/urls.py
@@ -1,9 +1,11 @@
+"""URLs for the Play app."""
 from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
-    url(r"^$", views.Index.as_view(), name="play_index"),
-    url(r"^archive$", views.Archive.as_view(), name="archive"),
-    url(r"^(?P<pk>\d+)/$", views.PlayPageDetail.as_view(), name="play_page"),
+    url(r'^$', views.Index.as_view(), name='play_index'),
+    url(r'^archive$', views.Archive.as_view(), name='archive'),
+    url(r'^(?P<pk>\d+)/$', views.PlayPageDetail.as_view(), name='play_page'),
 ]
