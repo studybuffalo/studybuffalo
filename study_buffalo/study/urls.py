13fe5c428f60ae5fc494f66b1c06ea560274e177
studybuffalo <studybuffalo@gmail.com>
2022-03-10 15:37:30 -0700

    Correcting URL order

--- study_buffalo/study/urls.py
+++ study_buffalo/study/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='study_index'),
     path('<int:pk>/', views.GuideDetail.as_view(), name='study_guide_page'),
+    path('', views.Index.as_view(), name='study_index'),
 ]
