89d3ebb3f71ff5e2cc5e45fa501108d7f0d1e8d5
studybuffalo <studybuffalo@gmail.com>
2022-03-10 10:51:04 -0700

    Making archive name more specific to avoid conflicts

--- study_buffalo/play/urls.py
+++ study_buffalo/play/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('archive/', views.Archive.as_view(), name='archive'),
+    path('archive/', views.Archive.as_view(), name='play_archive'),
     path('<int:pk>/', views.PlayPageDetail.as_view(), name='play_page'),
     path('', views.Index.as_view(), name='play_index'),
 ]
