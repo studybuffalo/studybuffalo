b54433963370b8869ae7a63c18f18266d38e7b3d
studybuffalo <studybuffalo@gmail.com>
2022-03-10 18:48:30 -0700

    Fixing URL parameter name

--- study_buffalo/substitutions/urls.py
+++ study_buffalo/substitutions/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('review/delete-entry/', views.delete_pend, name='sub_delete_entry'),
     path('review/retrieve-entries/', views.retrieve_entries, name='sub_retrieve_entry'),
     path('review/verify-entry/', views.verify, name='sub_verify_entry'),
-    path('review/<int:id>/', views.review, name='sub_review'),
+    path('review/<int:app_id>/', views.review, name='sub_review'),
     path('', views.dashboard, name='sub_dashboard'),
 ]
