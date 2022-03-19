7d40c339c9cce78deacd0965115d7d27401be68e
Study Buffalo <joshua@torrance.io>
2022-03-19 15:28:34 -0600

    Create update tests (#654)
    * Standardizing model format
    
    * Initial tests for the Update models
    
    * Removing unused datetime import
    
    Co-authored-by: studybuffalo <studybuffalo@gmail.com>

--- study_buffalo/updates/models.py
+++ study_buffalo/updates/models.py
@@ -5,50 +5,41 @@ import django.utils.timezone
 
 class Update(models.Model):
     """Defines a single slide for the front page carousel"""
-    # Model fields
     title = models.CharField(
         max_length=256,
     )
-
     background = models.ImageField(
         upload_to='home/update/',
         blank=True,
         null=True,
         help_text='Optional background image',
     )
-
     icon = models.ImageField(
         upload_to='home/update/',
         help_text='Image for the update icon',
     )
-
     html = models.TextField(
         blank=True,
         help_text='Any HTML to accompany the update',
     )
-
     start_date = models.DateField(
         default=django.utils.timezone.now,
         help_text='Date to start displaying the update',
     )
-
     end_date = models.DateField(
         blank=True,
         null=True,
         help_text='Date to stop displaying the update (leave blank for no end date)',
     )
-
     priority = models.SmallIntegerField(
         default=1,
         help_text='The priority order to show the slide',
     )
 
-    # Meta settings
     class Meta:
         ordering = ['priority', 'start_date']
         verbose_name = 'Update'
         verbose_name_plural = 'Updates'
 
-    # Methods
     def __str__(self):
         return self.title
---
+++ study_buffalo/updates/tests/test__models.py
@@ -0,0 +1,46 @@
+"""Tests for the Models module of the Updates app."""
+import pytest
+
+from updates import models
+
+
+pytestmark = pytest.mark.django_db
+
+
+def test__update__minimal_creation(updates_update):
+    """Tests minimal Update model creation."""
+    update_count = models.Update.objects.count()
+
+    models.Update.objects.create(
+        title='Update 1',
+        icon=updates_update.icon,
+        html='<html>Update 1</html>',
+        priority=1,
+    )
+
+    assert models.Update.objects.count() == update_count + 1
+
+
+def test__update__title__max_length():
+    """Confirms title field's max length in Update model."""
+    max_length = models.Update._meta.get_field('title').max_length
+
+    assert max_length == 256
+
+
+def test__update__priority__default():
+    """Confirms priority field's default in Update model."""
+    default = models.Update._meta.get_field('priority').default
+
+    assert default == 1
+
+
+def test__publication__str(updates_update):
+    """Tests for expected output of the Update __str__ method."""
+    update = models.Update.objects.create(
+        title='Update 1',
+        icon=updates_update.icon,
+        html='<html>Update 1</html>',
+        priority=1,
+    )
+    assert str(update) == 'Update 1'
