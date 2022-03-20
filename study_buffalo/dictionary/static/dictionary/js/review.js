ca7901a8f6ddac50a287847aec5ded2559355bab
studybuffalo <studybuffalo@gmail.com>
2022-03-20 12:22:32 -0600

    Fixing issues with JavaScript due to new JSON responses

--- study_buffalo/dictionary/static/dictionary/js/review.js
+++ study_buffalo/dictionary/static/dictionary/js/review.js
@@ -36,7 +36,7 @@ function SendMessage(msg, tag) {
       tag,
     };
 
-    Notification(title, options);
+    new Notification(title, options); // eslint-disable-line no-new
   }
 }
 
@@ -131,7 +131,7 @@ function RemovePendingWord(pendingID) {
   const CSRF = $('[name=csrfmiddlewaretoken]').val();
 
   $.ajaxSetup({
-    beforeSend: (xhr, settings) => {
+    beforeSend: (xhr) => {
       if (!this.crossDomain) {
         xhr.setRequestHeader('X-CSRFToken', CSRF);
       }
@@ -144,20 +144,23 @@ function RemovePendingWord(pendingID) {
       pending_id: pendingID,
     },
     type: 'POST',
-    success: (results) => {
-      if (results.success) {
+    success: (results, _, jqXHR) => {
+      if (jqXHR.status === 204) {
         // Remove this entry form the page
-        $(`#pending-${results.id}`).remove();
+        $(`#pending-${pendingID}`).remove();
 
         // Reduce the pending count by 1
         const $count = $('#word_count');
         $count.text(Number($count.text()) - 1);
 
         // Request to display additional entries
-        RetrieveEntries();
-      }
+        RetrieveEntries(); // eslint-disable-line no-use-before-define
 
-      SendMessage(results.message, 'delete-pending');
+        // Send message notifying of deletion
+        SendMessage(`Pending word ${pendingID} deleted`, 'delete-pending');
+      } else {
+        SendMessage(results.message, 'delete-pending');
+      }
     },
     error: (jqXHR, textStatus, errorThrown) => {
       SendMessage(
@@ -198,15 +201,15 @@ function AddWord(button, e) {
   $.ajax({
     url: 'add-new-word/',
     data: {
-      pendingID,
-      modelName,
+      pending_id: pendingID,
+      model_name: modelName,
       word,
       language,
-      dictionaryType,
-      dictionaryClass,
+      dictionary_type: dictionaryType,
+      dictionary_class: dictionaryClass,
     },
     type: 'POST',
-    beforeSend: (jqXHR, settings) => {
+    beforeSend: (jqXHR) => {
       // Setup the CSRF token for the POST request
       if (!this.crossDomain) {
         const CSRF = $('[name=csrfmiddlewaretoken]').val();
@@ -330,7 +333,7 @@ function RetrieveEntries() {
   const CSRF = $('[name=csrfmiddlewaretoken]').val();
 
   $.ajaxSetup({
-    beforeSend: (xhr, settings) => {
+    beforeSend: (xhr) => {
       if (!this.crossDomain) {
         xhr.setRequestHeader('X-CSRFToken', CSRF);
       }
@@ -352,10 +355,6 @@ function RetrieveEntries() {
       console.error(`${textStatus}: ${errorThrown}`);
     },
   });
-}
-
-function ExcludeWord(button) {
-  console.log('Excluding word stuff goes here');
 }
 
 function ClearDisplayedEntries() {
