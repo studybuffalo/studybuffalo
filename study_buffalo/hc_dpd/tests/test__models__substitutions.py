b48fe5c875e02d139c65593e0a895a007c2e7582
studybuffalo <studybuffalo@gmail.com>
2022-05-23 09:58:35 -0600

    Adding details on sub tests

---
+++ study_buffalo/hc_dpd/tests/test__models__substitutions.py
@@ -0,0 +1,6 @@
+"""Tests for Substitutions model of HC DPD app.
+
+    No tests written for this module yet as I am  not sure what it will
+    look like in final state. Once confirmed, can write the required
+    tests.
+"""
