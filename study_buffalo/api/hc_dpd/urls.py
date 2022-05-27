961aeefaa234b5ed4c238a21db340cfd1b03b689
Study Buffalo <studybuffalo@gmail.com>
2022-05-27 17:35:14 -0600

    Correcting URL and app names

--- study_buffalo/api/hc_dpd/urls.py
+++ study_buffalo/api/hc_dpd/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from api.hc_dpd import views
 
 
-app_name = 'hc_dpd_api_v1'
+app_name = 'hc_dpd_v1'
 
 urlpatterns = [
     # Endpoints to manage checksums
-    path('checksum/', views.ChecksumList.as_view()),
+    path('checksum/', views.ChecksumList.as_view(), name='checksum_list'),
 
     # Endpoints to modify database
-    path('upload/', views.UploadHCDPDData.as_view()),
+    path('upload/', views.UploadHCDPDData.as_view(), name='upload_hc_dpd_data'),
 ]
